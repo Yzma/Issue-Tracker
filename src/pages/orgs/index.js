4c6a1e48ba9f57f1cfbf8c9bc3984ed0cc9ee251
jpared3s <jparedes.wall@gmail.com>
2023-03-17 15:35:11 -0400

    intial organization page commit

--- src/pages/orgs/index.js
+++ src/pages/orgs/index.js
@@ -2,13 +2,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import Header from "@/components/Header";
 import { useRouter } from 'next/router'
+import OrganizationContainer from '@/components/OrganizationContainer';
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Org() {
+  const orgName = "Bug Zapper";
+  const bio = "Z";
+  const projects = [
+    { id: 1, name: "Tweeter App", description: "Twitter clone app", updated_at: "March 17 2023" },
+    { id: 2, name: "Scheduler App", description: "Scheduler Appointment App", updated_at: "March 17 2023" },
+  ];
+  const users = ["Julian Paredes", "Andrew Caruso"]
   return (
     <>
       <Head>
@@ -18,7 +26,13 @@ export default function Org() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <p>Orgs view here</p>
+      <Header />
+      <div>
+        <OrganizationContainer orgName={orgName} bio={bio}/>
+        </div>
+        <div>
+        {/* <OrganizationContainer projects={projects} /> */}
+        </div>
       </main>
     </>
   )
