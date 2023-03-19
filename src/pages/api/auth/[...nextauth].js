88e349aa049fa067ec5886ab5ab72a4b7987c722
Yzma <10090913+Yzma@users.noreply.github.com>
2023-03-19 13:25:51 -0400

    Switched to using jose to encrypt and decrypt keys for user creation

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -6,17 +6,13 @@ import GoogleProvider from "next-auth/providers/google"
 import CustomPrismaAdapter from "@/lib/prisma/prisma-adapter"
 import prisma from "@/lib/prisma/prisma"
 
-import { setCookie } from 'cookies-next';
-import jwt from 'jsonwebtoken';
+import { setCookie } from "cookies-next"
 
-import { NEW_USER_COOKIE } from '@/lib/constants'
+import { NEW_USER_COOKIE } from "@/lib/constants"
+
+import { generateEncryptedJwt } from "@/lib/jwt"
 
 const DEFAULT_MAX_AGE = 24 * 60 * 60 // 1 day
-
-//https://next-auth.js.org/configuration/callbacks#jwt-callback - at the bottom of the page
-// The session object is not persisted server side, even when using database sessions - only data such as the session token, the user, and the expiry time is stored in the session table.
-// If you need to persist session data server side, you can use the accessToken returned for the session as a key - and connect to the database in the session() callback to access it. Session accessToken values do not rotate and are valid as long as the session is valid.
-// If using JSON Web Tokens instead of database sessions, you should use the User ID or a unique key stored in the token (you will need to generate a key for this yourself on sign in, as access tokens for sessions are not generated when using JSON Web Tokens).
 
 export const authOptions = (req, res) => {
   return {
@@ -38,57 +34,72 @@ export const authOptions = (req, res) => {
         }
       })
     ],
-  
+
     callbacks: {
       async signIn({ user, account, profile, email, credentials }) {
-        // console.log("SIGN IN CALLED")
-        // console.log("user", user)
-        // console.log("account", account)
-        // console.log("profile", profile)
-        // console.log("email", email)
-        // console.log("credentials", credentials)
-
         // FIRST TIME LOGIN
         if (!user.namespace) {
-          console.log("SET COOKIE")
-
-          return new Promise((resolve, reject) => {
-            jwt.sign({ id: user.id }, process.env.NEXTAUTH_SECRET, (err, token) => {
-              if(err) {
-                return reject(err)
-              }
-              return resolve(token)
-            })
-          }).then(token => {
-            setCookie(NEW_USER_COOKIE, token, { req, res, maxAge: DEFAULT_MAX_AGE })
-            return { redirect: '/finish', stayLoggedIn: true }
-          }).catch(err => {
-            console.error("Error signing JSON Web token", err)
-            return { redirect: '/500', stayLoggedIn: false } // Something went wrong here
-          })
+          return { redirect: "/finish", stayLoggedIn: true }
         }
-   
+
+        console.log("User already has namespace")
         return { redirect: `/${user.namespace.name}`, stayLoggedIn: true }
       },
 
       async session({ session, token, user }) {
-        // console.log("session called")
-        // console.log(" ")
-        // console.log('callback session')
-        // console.log('session', session)
-        // console.log('token', token)
-        // console.log('user', user)
-
         session.user.id = user.id
         session.namespace = user.namespace?.name
 
         // session.user.settings = {
         //   colorScheme: user.settings.colorScheme
         // }
-
         return session
       }
     },
+
+    events: {
+      async signIn(message) {
+        console.log("SIGN IN ")
+        // console.log(message)
+        if (!message.user.namespace) {
+          return new Promise((resolve, reject) => {
+            const sessionToken = message.cookies.pop()
+
+            const toSign = {
+              id: message.user.id,
+              session: sessionToken
+            }
+            console.log("toSign = ", toSign)
+
+            try {
+              return resolve(generateEncryptedJwt("testsub", toSign))
+            } catch (e) {
+              console.log("JOSE error", e)
+              return reject(e)
+            }
+          })
+            .then((token) => {
+              // console.log("MESSAGE COOKIES: ", message.cookies)
+              // console.log()
+              // console.log("TOKEN = ", token)
+
+              setCookie(NEW_USER_COOKIE, token, {
+                req,
+                res,
+                maxAge: DEFAULT_MAX_AGE
+              })
+
+              //SELECT * FROM "Session" WHERE "userId"='clfes6o2g0000qqbw070cmzaq' order by expires desc limit 1;
+
+              return "/finish"
+            })
+            .catch((err) => {
+              console.error("Error signing JSON Web token", err)
+              return "/500"
+            })
+        }
+      }
+    }
   }
 }
 
