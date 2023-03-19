1506c81a8aa73cda6ea0452b339a843150869931
Yzma <10090913+Yzma@users.noreply.github.com>
2023-03-19 16:39:40 -0400

    Implement backend api route to create new organizations

---
+++ src/pages/api/organization/index.js
@@ -0,0 +1,50 @@
+import prisma from "@/lib/prisma/prisma"
+
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/pages/api/auth/[...nextauth]"
+
+import { OrganizationNameCreationSchema } from "@/lib/yup-schemas"
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { name } = req.body
+
+    try {
+      await OrganizationNameCreationSchema.validate({ name })
+    } catch (e) {
+      console.log("name error: ", name)
+      return res.status(400).json({ error: "Invalid name" })
+    }
+
+    const session = await getServerSession(req, res, authOptions(req, res))
+    if (!session) {
+      return res.status(400).json({ error: "Invalid session" })
+    }
+
+    return await prisma.organization
+      .create({
+        data: {
+          name,
+          userId: session.user.id,
+          namespace: {
+            create: {
+              name
+            }
+          }
+        }
+      })
+      .then((result) => {
+        console.log("API RESULT: ", result)
+        return res.status(200).json({ name: "Hello?" })
+      })
+      .catch((err) => {
+        // TODO: Check individual error codes from prisma. Check if the name already exists, if the user already has a namespace, etc
+        console.log("error: ", err)
+        return res
+          .status(400)
+          .json({ error: "Error creating entry in database" })
+      })
+  } else {
+    res.json({ error: "Not supported" })
+  }
+}
