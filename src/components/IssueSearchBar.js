5cbd8cff1c447631ccea19611ecdeae04f6005bf
jpared3s <jparedes.wall@gmail.com>
2023-03-19 14:23:03 -0400

    search button styling

--- src/components/IssueSearchBar.js
+++ src/components/IssueSearchBar.js
@@ -1,22 +1,34 @@
 import { useState } from 'react';
 import styles from '@/styles/IssueSearchBar.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'; // Import the search icon (fa-magnifying-glass)
+
 
 const SearchBar = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
-    onSearch(e.target.value);
+  };
+
+  const handleSearchClick = () => {
+    onSearch(searchTerm);
   };
 
   return (
-    <input
-      type="text"
-      placeholder="Search Label"
-      value={searchTerm}
-      onChange={handleChange}
-      className={styles.searchBar}
-    />
+    <div className={styles.container}>
+      <input
+        type="text"
+        placeholder="Search Label"
+        value={searchTerm}
+        onChange={handleChange}
+        className={styles.searchBar}
+      />
+    <button onClick={handleSearchClick} className={styles.searchButton}>
+      <FontAwesomeIcon icon={faSearch} />
+      <span> Search</span>
+    </button>
+    </div>
   );
 };
 
