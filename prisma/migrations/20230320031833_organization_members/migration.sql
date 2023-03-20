9e188f6e2425957c1c60bc933ede5ef588efdb07
Yzma <10090913+Yzma@users.noreply.github.com>
2023-03-19 23:19:59 -0400

    Implemneted OrganizationMember model

---
+++ prisma/migrations/20230320031833_organization_members/migration.sql
@@ -0,0 +1,25 @@
+-- CreateEnum
+CREATE TYPE "OreganizationRole" AS ENUM ('Owner', 'Admin', 'Maintainer', 'User');
+
+-- AlterTable
+ALTER TABLE "OrganizationInvitation" ADD COLUMN     "role" "OreganizationRole" NOT NULL DEFAULT 'User';
+
+-- CreateTable
+CREATE TABLE "OrganizationMember" (
+    "id" TEXT NOT NULL,
+    "role" "OreganizationRole" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "organizationId" TEXT NOT NULL,
+
+    CONSTRAINT "OrganizationMember_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "OrganizationMember_userId_organizationId_key" ON "OrganizationMember"("userId", "organizationId");
+
+-- AddForeignKey
+ALTER TABLE "OrganizationMember" ADD CONSTRAINT "OrganizationMember_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrganizationMember" ADD CONSTRAINT "OrganizationMember_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
