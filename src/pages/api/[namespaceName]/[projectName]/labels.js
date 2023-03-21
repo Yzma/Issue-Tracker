a160b6367b6106b3615212bc9f304f50f3d670dd
Yzma <10090913+Yzma@users.noreply.github.com>
2023-03-21 11:58:06 -0400

    Implement GET route to get all projects labels

--- src/pages/api/[namespaceName]/[projectName]/labels.js
+++ src/pages/api/[namespaceName]/[projectName]/labels.js
@@ -5,9 +5,35 @@ import { getServerSession } from "@/lib/sessions"
 import { LabelCreationSchema } from "@/lib/yup-schemas"
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
 
-    const { namespaceName, projectName } = req.query
+  const { namespaceName, projectName } = req.query
+
+  if (req.method === "GET") {
+
+    return await prisma.label
+      .findMany({
+        where: {
+          project: {
+            name: projectName,
+            namespace: {
+              name: namespaceName
+            }
+          }
+        }
+      })
+      .then((result) => {
+        console.log("API RESULT: ", result)
+        return res.status(200).json({ result: result })
+      })
+      .catch((err) => {
+        // TODO: Check individual error codes from prisma. Check if the name already exists, if the user already has a namespace, etc
+        console.log("error: ", err)
+        return res
+          .status(400)
+          .json({ error: "Error creating entry in database" })
+      })
+
+  } else if (req.method === "POST") {
     const { name, description, color } = req.body
 
     let schemaResult
@@ -43,22 +69,22 @@ export default async function handler(req, res) {
     }
 
     if (foundNamespace.userId) {
-
       if (foundNamespace.userId != session.user.id) {
-        return res.status(400).json({ error: "User is not who they say they are" })
+        return res
+          .status(400)
+          .json({ error: "User is not who they say they are" })
       }
-
     } else if (foundNamespace.organizationId) {
-
-      const foundOrganizationMember = await prisma.organizationMember.findUnique({
-        where: {
-          userId_organizationId: {
-            userId: session.user.id,
-            organizationId: foundNamespace.organizationId
+      const foundOrganizationMember =
+        await prisma.organizationMember.findUnique({
+          where: {
+            userId_organizationId: {
+              userId: session.user.id,
+              organizationId: foundNamespace.organizationId
+            }
           }
-        }
-      })
-  
+        })
+
       if (!foundOrganizationMember) {
         return res.status(400).json({ error: "Not in the organization" })
       }
@@ -68,14 +94,14 @@ export default async function handler(req, res) {
       if (foundOrganizationMember.role != "Owner") {
         return res.status(400).json({ error: "User isn't an owner" })
       }
-
     } else {
       // Note: There is a constraint on the database top not allow this to happen so this check will probably be removed in the future
       console.log("ERROR: Namespace does not have a user or and organization")
       return res.status(400).json({ error: "Debug: This should never happen" })
     }
-    
-    return await prisma.project.update({
+
+    return await prisma.project
+      .update({
         where: {
           namespaceId_name: {
             name: projectName,
