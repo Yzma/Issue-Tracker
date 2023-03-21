1ab3d04f1a9cec78202ddbe62680d22b6ccff759
Yzma <10090913+Yzma@users.noreply.github.com>
2023-03-21 00:42:49 -0400

    Update getServerSideProps for issues index page to fetch correct issues

--- src/pages/[namespaceName]/[projectName]/[issues]/index.js
+++ src/pages/[namespaceName]/[projectName]/[issues]/index.js
@@ -8,47 +8,9 @@ import SearchBar from "@/components/IssueSearchBar";
 import { useState } from "react";
 import prisma from "@/lib/prisma/prisma";
 
-//Fake data for now
-// const issuesData = [
-//   {
-//     title: 'Font Awesome Install Bug',
-//     issueNumber: 1,
-//     description: 'Unable to rend icons on webpage',
-//     status: 'Open',
-//     createdAt: '2023-03-01',
-//     updatedAt: '2023-03-02',
-//   },
-//   {
-//     title: 'User Login Bug',
-//     issueNumber: 2,
-//     description: 'User unable to log in',
-//     status: 'Closed',
-//     createdAt: '2023-03-16',
-//     updatedAt: '2023-03-17',
-//   },
-// ];
-
-//commented out the hardcoded data above and below is the fetch for issue data
-export async function getServerSideProps() {
-  const issuesData = await prisma.issue.findMany({
-    include: {
-      labels: true,
-    },
-  });
-
-  return {
-    props: {
-      issuesData: issuesData.map((issue) => ({
-        ...issue,
-        createdAt: issue.createdAt.toISOString(),
-        updatedAt: issue.updatedAt.toISOString(),
-      })),
-    },
-  };
-}
-
 //added issuesData as a prop
 export default function Issues({ issuesData }) {
+  console.log(issuesData)
   const [filteredIssues, setFilteredIssues] = useState(issuesData);
 
   const handleSearch = (searchTerm) => {
@@ -84,4 +46,33 @@ export default function Issues({ issuesData }) {
       </main>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { namespaceName, projectName } = context.query
+  const issuesData = await prisma.issue.findMany({
+    where: {
+      project: {
+        name: projectName,
+        namespace: {
+          name: namespaceName
+        }
+      }
+    },
+    include: {
+      labels: true
+    },
+  });
+
+  console.log(issuesData)
+
+  return {
+    props: {
+      issuesData: issuesData.map((issue) => ({
+        ...issue,
+        createdAt: issue.createdAt.toISOString(),
+        updatedAt: issue.updatedAt.toISOString(),
+      })),
+    },
+  };
 }
