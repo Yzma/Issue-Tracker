170e99201a9470da4f67be97fdc89a2c20d0cae7
Yzma <10090913+Yzma@users.noreply.github.com>
2023-03-20 23:06:12 -0400

    Ensure username and organization name are unique

---
+++ prisma/migrations/20230321030539_organization_name_and_username_unique/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `Organization` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_name_key" ON "Organization"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
