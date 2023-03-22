c6937103eb0866809d1c6063a0ae87f0d7f835fb
Yzma <10090913+Yzma@users.noreply.github.com>
2023-03-22 16:59:36 -0400

    Correct header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
-import React from "react";
-import styles from '../styles/Header.module.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBug } from "@fortawesome/free-solid-svg-icons";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { useSession, signIn, signOut } from "next-auth/react";
+import React from "react"
+import Link from "next/link"
+import styles from "../styles/Header.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBug } from "@fortawesome/free-solid-svg-icons"
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const Header = () => {
-  const { data: session } = useSession();
+  const { data: session } = useSession()
 
   console.log(session)
+
+  if (!session) return
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
@@ -18,10 +21,10 @@ const Header = () => {
       <nav className={styles.nav}>
         <ul>
           <li>
-            <a href="/issues">Issues</a>
+            <Link href="/issues">Issues</Link>
           </li>
           <li>
-            <a href="/orgs">Organizations</a>
+            <Link href="/orgs">Organizations</Link>
           </li>
           <li>
             <a href={`/${session?.namespace}`}>{session?.namespace}</a>
@@ -29,11 +32,21 @@ const Header = () => {
           <li>
             {session ? (
               <>
-                <button className={styles.signInOutButton} onClick={() => signOut()}>Sign Out</button>
+                <button
+                  className={styles.signInOutButton}
+                  onClick={() => signOut()}
+                >
+                  Sign Out
+                </button>
               </>
             ) : (
               <>
-                <button className={styles.signInOutButton} onClick={() => signIn()}>Sign In</button>
+                <button
+                  className={styles.signInOutButton}
+                  onClick={() => signIn()}
+                >
+                  Sign In
+                </button>
               </>
             )}
           </li>
@@ -43,10 +56,10 @@ const Header = () => {
         </ul>
       </nav>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
 
 // import React from "react";
 // import styles from '../styles/Header.module.css';
