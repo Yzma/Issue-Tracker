6ab91815ef46ebbe1770b48002c38b4999cd13bc
jpared3s <jparedes.wall@gmail.com>
2023-03-25 13:13:25 -0400

    clean up

--- src/components/IssueButtons.js
+++ src/components/IssueButtons.js
@@ -5,23 +5,6 @@ import { faTag } from '@fortawesome/free-solid-svg-icons';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { useRouter } from "next/router"
 
-
-// const IssueButtons = ({onSearch, path}) => {
-//   const router = useRouter()
-//   return (
-//     <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2 my-4"> {/* Updated className */}
-//       <SearchBar onSearch={onSearch} />
-//       <button className="btn bg-green-600 hover:bg-green-500 text-white" onClick={() => router.push(`${path}/labels`)}>
-//         <FontAwesomeIcon icon={faTag} />
-//         <span> Label</span>
-//       </button>
-//       <button className="btn bg-green-600 hover:bg-green-500 text-white" onClick={() => router.push(`${path}/new`)}>
-//         <FontAwesomeIcon icon={faPlus} />
-//         <span > Create Issue</span>
-//       </button>
-//     </div>
-//   );
-// };
 const IssueButtons = ({onSearch, path}) => {
   const router = useRouter()
   return (
