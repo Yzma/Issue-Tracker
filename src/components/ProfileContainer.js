81c612d608c2a3882caa91dc6f36c96e141b4864
jpared3s <jparedes.wall@gmail.com>
2023-03-24 21:38:07 -0400

    user page tailwind styling

--- src/components/ProfileContainer.js
+++ src/components/ProfileContainer.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import styles from "../styles/ProfileContainer.module.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const ProfileContainer = ({ username, bio }) => {
   return (
-    <section className={styles.container}>
-      <div className={styles.profileImage}>
-      <FontAwesomeIcon icon={faUser} />
+    <section className="bg-white shadow-md rounded-lg p-6">
+      <div className="flex items-center justify-center w-16 h-16 mb-4 bg-gray-200 text-gray-600 rounded-full">
+        <FontAwesomeIcon icon={faUser} className="text-2xl" />
       </div>
-      <h2>{username}</h2>
-      <p>{bio}</p>
+      <h2 className="text-xl font-semibold mb-2">{username}</h2>
+      <p className="text-gray-500">{bio}</p>
     </section>
   );
 };
