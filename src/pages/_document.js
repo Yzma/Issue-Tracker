771c4ca125bf6fabad879e04891d7a91c42d5feb
Yzma <10090913+Yzma@users.noreply.github.com>
2023-03-24 21:29:40 -0400

    Refactor project creation page with tailwind

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head/>
-      <body>
+      <body className="font-inter antialiased bg-slate-100 text-slate-600">
         <Main />
         <NextScript />
       </body>
