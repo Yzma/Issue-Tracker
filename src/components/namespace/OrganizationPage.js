49cbc3e784bb315b9d40ff40a35ffffe7a566877
jpared3s <jparedes.wall@gmail.com>
2023-03-27 17:16:31 -0400

    removed buttons from org page

--- src/components/namespace/OrganizationPage.js
+++ src/components/namespace/OrganizationPage.js
@@ -28,8 +28,8 @@ const OrganizationPage = ({ props }) => {
 
       <OrganizationBelowNavbar namespaceName={namespaceName} selected={"projects"} />
 
-      <div className={layoutStyles.projectSection}>
-        <Tabs activeTab={activeTab} onTabClick={handleTabClick} />
+      <div className={`${layoutStyles.projectSection} mt-4`}> {/* Add the mt-4 class here */}
+        {/* <Tabs activeTab={activeTab} onTabClick={handleTabClick} /> */}
         {activeTab === "projects" && (
           <OrgProjectSection projects={props.namespace.projects} />
         )}
@@ -46,3 +46,51 @@ const OrganizationPage = ({ props }) => {
 }
 
 export default OrganizationPage
+// import React, { useState } from "react"
+// import { useRouter } from "next/router"
+// import layoutStyles from "@/styles/usersLayout.module.css"
+// import Head from "next/head"
+
+// import OrgProjectSection from "@/components/OrgProjectSection"
+// import Tabs from "@/components/OrgPageTabs"
+// import UserSection from "@/components/OrgUserSection"
+// import OrganizationBelowNavbar from "../navbar/OrganizationBelowNavbar"
+
+// const OrganizationPage = ({ props }) => {
+//   const router = useRouter()
+//   const { namespaceName } = router.query
+//   console.log("Org props ", props)
+
+//   const [activeTab, setActiveTab] = useState("projects")
+
+//   const handleTabClick = (tab) => {
+//     setActiveTab(tab)
+//   }
+
+//   return (
+//     <>
+//       <Head>
+//         <title>{props.data.name}</title>
+//         <meta name="description" content="TODO: Description?" />
+//       </Head>
+
+//       <OrganizationBelowNavbar namespaceName={namespaceName} selected={"projects"} />
+
+//       <div className={layoutStyles.projectSection}>
+//         {/* <Tabs activeTab={activeTab} onTabClick={handleTabClick} /> */}
+//         {activeTab === "projects" && (
+//           <OrgProjectSection projects={props.namespace.projects} />
+//         )}
+//         {activeTab === "users" && (
+//           <UserSection
+//             users={props.data.members.map(
+//               (member) => member.user.username
+//             )}
+//           />
+//         )}
+//       </div>
+//     </>
+//   )
+// }
+
+// export default OrganizationPage
