a72a8576d4d226960ba48325e37f94743646a82e
Yzma <10090913+Yzma@users.noreply.github.com>
2023-03-26 22:31:58 -0400

    Small cleanup for labels page

--- src/components/LabelList.js
+++ src/components/LabelList.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import styles from '@/styles/LabelList.module.css';
+import React from "react"
+import styles from "@/styles/LabelList.module.css"
 
 const LabelList = ({ labels }) => {
   return (
-    <div className={styles.labelListContainer}>
-      <div className={styles.labelListWrapper}>
-        <table className={styles.table}>
-          <thead>
-            <tr className={styles.header}>
-              <th className={styles.cell}>Name</th>
-              <th className={styles.cell}>Description</th>
-            </tr>
-          </thead>
-          <tbody>
-            {labels.map((label, index) => (
-              <tr key={index}>
-                <td className={styles.cell}>{label.name}</td>
-                <td className={styles.cell}>{label.description}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-};
+    <table className="min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
+        <tr>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Title
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Label
+          </th>
+        </tr>
+      </thead>
+      <tbody className="bg-white divide-y divide-gray-200">
+        {labels.map((label, index) => (
+          <tr key={index}>
+            <td className={styles.cell}>{label.name}</td>
+            <td className={styles.cell}>{label.description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
-export default LabelList;
+export default LabelList
