2cdf9911a28acc4bd38348546a825986c1405c5b
Yzma <10090913+Yzma@users.noreply.github.com>
2023-04-05 17:48:35 -0400

    Reimplement the signin process by making it easier to link accounts, less queries, and better readability

--- src/middleware.js
+++ src/middleware.js
@@ -6,20 +6,20 @@ export async function middleware(request) {
   
   const { pathname } = request.nextUrl
 
-  if(pathname !== "/finish") {
-    // TODO: Store session cookie as well so users cant navigate away?
-    if(request.cookies.has(NEW_USER_COOKIE)) {
-      const response = NextResponse.next()
-      response.cookies.set(NEW_USER_COOKIE, "", { maxAge: -1 })
-      response.cookies.set(NEXT_AUTH_SESSION_COOKIE, "", { maxAge: -1 })
-      return response
-    }
-  } else {
-    if(!request.cookies.has(NEW_USER_COOKIE)) {
-      request.nextUrl.pathname = `/`;
-      return NextResponse.redirect(request.nextUrl);
-    }
-  }
+  // if(pathname !== "/finish") {
+  //   // TODO: Store session cookie as well so users cant navigate away?
+  //   if(request.cookies.has(NEW_USER_COOKIE)) {
+  //     const response = NextResponse.next()
+  //     response.cookies.set(NEW_USER_COOKIE, "", { maxAge: -1 })
+  //     response.cookies.set(NEXT_AUTH_SESSION_COOKIE, "", { maxAge: -1 })
+  //     return response
+  //   }
+  // } else {
+  //   if(!request.cookies.has(NEW_USER_COOKIE)) {
+  //     request.nextUrl.pathname = `/`;
+  //     return NextResponse.redirect(request.nextUrl);
+  //   }
+  // }
 }
 
 export const config = {
@@ -34,40 +34,3 @@ export const config = {
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 }
-
-// export async function middleware(request) {
-  
-//   const { pathname } = request.nextUrl
-//   console.log('MIDDLEWARE PATH: ', pathname)
-//   // TODO: Delete cookie if navigating away
-
-//   if(pathname === "/finish") {
-//     console.log('running if statement for middleware')
-//     if(request.cookies.has(process.env.NEW_USER_COOKIE)) {
-//       console.log('1 here')
-//       // request.cookies.set(process.env.NEW_USER_COOKIE, "", { maxAge: -1 })
-//       // request.cookies.set("next-auth.session-token", "")
-
-//       const response = NextResponse.next()
-//       // response.cookies.set(process.env.NEW_USER_COOKIE, "", { maxAge: -1 })
-//       // response.cookies.set("next-auth.session-token", "", { maxAge: -1 })
-
-//       // response.cookies.set('vercel', 'fast')
-//       // response.cookies.set({
-//       //   name: 'vercel',
-//       //   value: 'fast',
-//       //   path: '/test',
-//       // })
-//       // cookie = response.cookies.get('vercel')
-//       // console.log(cookie) // => { name: 'vercel', value: 'fast', Path: '/test' }
-//       // // The outgoing response will have a `Set-Cookie:vercel=fast;path=/test` header.
-    
-//       return response
-
-//     } else {
-//       console.log('2 here')
-//       request.nextUrl.pathname = `/maintenance`;
-//       return NextResponse.redirect(request.nextUrl);
-//     }
-//   }
-// }
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -6,17 +6,20 @@ import GoogleProvider from "next-auth/providers/google"
 import CustomPrismaAdapter from "@/lib/prisma/prisma-adapter"
 import prisma from "@/lib/prisma/prisma"
 
-import { setCookie } from "cookies-next"
-
-import { NEW_USER_COOKIE } from "@/lib/constants"
-
+import jwt from "jsonwebtoken"
 import { generateEncryptedJwt } from "@/lib/jwt"
+import { deleteCookie, setCookie, getCookie } from "cookies-next"
 
-const DEFAULT_MAX_AGE = 24 * 60 * 60 // 1 day
+import {
+  NEW_USER_COOKIE,
+  NEXT_AUTH_CALLBACK_URL_COOKIE,
+  NEXT_AUTH_SESSION_COOKIE
+} from "@/lib/constants"
 
 export const authOptions = (req, res) => {
   return {
     adapter: CustomPrismaAdapter(prisma),
+    // TODO: Other providers to add: Gitlab, LinkedIn
     providers: [
       GitHubProvider({
         clientId: process.env.GITHUB_CLIENT_ID,
@@ -35,74 +38,32 @@ export const authOptions = (req, res) => {
       })
     ],
     pages: {
-      signIn: "/signin",
+      signIn: "/signin"
     },
 
     callbacks: {
-      async signIn({ user, account, profile, email, credentials }) {
-        // FIRST TIME LOGIN
-        if (!user.namespace) {
-          return { redirect: "/finish", stayLoggedIn: true }
-        }
+      async signIn(obj) {
+        console.log("signIn obj: ", obj)
 
-        console.log("User already has namespace")
-        return { redirect: `/${user.namespace.name}`, stayLoggedIn: true }
-      },
-
-      async session({ session, token, user }) {
-        session.user.id = user.id
-        session.namespace = user.namespace?.name
-
-        // session.user.settings = {
-        //   colorScheme: user.settings.colorScheme
-        // }
-        return session
-      }
-    },
-
-    events: {
-      async signIn(message) {
-        console.log("SIGN IN ")
-        console.log(message)
-        if (!message.user.namespace) {
-          return new Promise((resolve, reject) => {
-            const sessionToken = message.cookies.pop()
-
-            const toSign = {
-              id: message.user.id,
-              session: sessionToken
-            }
-            // console.log("toSign = ", toSign)
-
-            try {
-              return resolve(generateEncryptedJwt("testsub", toSign))
-            } catch (e) {
-              console.log("JOSE error", e)
-              return reject(e)
-            }
-          })
-            .then((token) => {
-              // console.log("MESSAGE COOKIES: ", message.cookies)
-              // console.log()
-              // console.log("TOKEN = ", token)
-
-              setCookie(NEW_USER_COOKIE, token, {
-                req,
-                res,
-                maxAge: DEFAULT_MAX_AGE
-              })
-
-              //SELECT * FROM "Session" WHERE "userId"='clfes6o2g0000qqbw070cmzaq' order by expires desc limit 1;
-
+        // If the user doesn't have a namespace yet, then their still in the process of creating their account
+        if (!obj.user.namespace) {
+          return await setupUserOnboarding(obj, req, res)
+            .then((result) => {
               return "/finish"
             })
-            .catch((err) => {
-              console.error("Error signing JSON Web token", err)
+            .catch((e) => {
+              console.log("Error during onboarding: ", e)
               return "/500"
             })
         }
+        return true
+      },
 
-        return `/${message.user.namespace.name}`
+      async session({ session, token, user }) {
+        console.log("Session callback")
+        session.user.id = user.id // TODO: See if this is already set
+        session.namespace = user.namespace?.name
+        return session
       }
     }
   }
@@ -111,3 +72,288 @@ export const authOptions = (req, res) => {
 export default async function auth(req, res) {
   return await NextAuth(req, res, authOptions(req, res))
 }
+
+/* If it's a users first time signing in:
+    1. Create a User object with await prisma.user.create({}) - We already have the info needed from obj.profile
+    2. set a cookie indicating that the user needs to finish setup
+    3. In Middleware.js - Redirect the user to /finish if they have the cookie
+    4. Note: if the user deletes the cookie and signs in again, then just try-catch a user being created and if it fails, the user already tried before
+*/
+
+// TODO: What if the user changes their email?
+
+async function setupUserOnboarding(userObj, req, res) {
+  const { user, account, profile } = userObj
+
+  return prisma
+    .$transaction(async (tx) => {
+      const accountInfo = {
+        type: account.type,
+        provider: account.provider,
+        providerAccountId: account.providerAccountId,
+        refresh_token: account.refresh_token,
+        access_token: account.access_token,
+        expires_at: account.expires_at,
+        token_type: account.token_type,
+        scope: account.scope,
+        id_token: account.id_token,
+        session_state: account.session_state
+      }
+
+      if (!profile.email) {
+        throw new Error(`profile is missing an email`)
+      }
+
+      const upsertUser = await tx.user.upsert({
+        where: {
+          email: profile.email
+        },
+        update: {},
+        create: {
+          name: user.name,
+          email: user.email,
+          emailVerified: user.emailVerified,
+          image: user.image
+        }
+      })
+
+      const newAccount = await tx.account.upsert({
+        where: {
+          provider_providerAccountId: {
+            providerAccountId: account.providerAccountId,
+            provider: account.provider
+          }
+        },
+        update: {
+          ...accountInfo
+        },
+        create: {
+          ...accountInfo,
+          user: {
+            connect: {
+              id: upsertUser.id
+            }
+          }
+        }
+      })
+
+      console.log("Upsert result: ", newAccount)
+      return newAccount
+    })
+    .then((result) => {
+      console.log()
+      console.log("Result from promise: ", result)
+      const token = jwt.sign({ data: result.userId }, process.env.NEXTAUTH_SECRET, {
+        expiresIn: "1h"
+      })
+      setCookie(NEW_USER_COOKIE, token, { req, res })
+    })
+}
+
+
+/////////////////////////////////////////Version 2?
+
+// return prisma
+// .$transaction(async (tx) => {
+//   const accountInfo = {
+//     type: account.type,
+//     provider: account.provider,
+//     providerAccountId: account.providerAccountId,
+//     refresh_token: account.refresh_token,
+//     access_token: account.access_token,
+//     expires_at: account.expires_at,
+//     token_type: account.token_type,
+//     scope: account.scope,
+//     id_token: account.id_token,
+//     session_state: account.session_state
+//   }
+
+//   const newAccount = await tx.account.upsert({
+//     where: {
+//       providerAccountId: account.providerAccountId,
+//       provider: account.provider
+//     },
+//     update: {
+//       ...accountInfo
+//     },
+//     create: {
+//       ...accountInfo,
+//       user: {
+//         name: user.name,
+//         email: user.email,
+//         emailVerified: user.emailVerified,
+//         image: user.image
+//       }
+//     }
+//   })
+
+  // const upsertUser = await tx.user.upsert({
+  //   where: {
+  //     email: user.email
+  //   },
+  //   update: {},
+  //   create: {
+  //     name: user.name,
+  //     email: user.email,
+  //     emailVerified: user.emailVerified,
+  //     image: user.image
+  //   }
+  // })
+  
+
+//   const userTransaction = await tx.user.update({
+//     where: {
+//       id: upsertUser.id
+//     },
+
+//     data: {
+//       accounts: {
+
+//       }
+//     },
+
+//     create: {
+//       name: userObj.user.name,
+//       email: userObj.user.email,
+//       emailVerified: userObj.user.emailVerified,
+//       image: userObj.user.image,
+//       accounts: {
+//         create: {
+//           ...profile
+//         }
+//       }
+//     },
+
+//     update: {
+//       accounts: {
+//         upsert: {
+//           where: {
+//             provider_userId: {
+//               userId: userObj.user.id,
+//               provider: profile.provider
+//             }
+//           },
+
+//           create: {
+//             ...profile
+//           },
+
+//           update: {
+//             ...profile
+//           }
+//         }
+//       }
+//     }
+//   })
+
+
+/////////////////////////////////Version 1
+
+// async function setupUserOnboardingV1(userObj, req, res) {
+//   return prisma.$transaction(async (tx) => {
+
+//     const profile = {
+//       type: userObj.account.type,
+//       provider: userObj.account.provider,
+//       providerAccountId: userObj.account.providerAccountId,
+//       refresh_token: userObj.account.refresh_token,
+//       access_token: userObj.account.access_token,
+//       expires_at: userObj.account.expires_at,
+//       token_type: userObj.account.token_type,
+//       scope: userObj.account.scope,
+//       id_token: userObj.account.id_token,
+//       session_state: userObj.account.session_state,
+//     }
+
+//     const userTransaction = await tx.user.upsert({
+//       where: {
+//         email: userObj.user.email
+//       },
+
+//       create: {
+//         name: userObj.user.name,
+//         email: userObj.user.email,
+//         emailVerified: userObj.user.emailVerified,
+//         image: userObj.user.image,
+//         accounts: {
+//           create: {
+//             ...profile
+//           }
+//         }
+//       },
+
+//       update: {
+//         accounts: {
+//           upsert: {
+//             where: {
+//               provider_userId: {
+//                 userId: userObj.user.id,
+//                 provider: profile.provider
+//               }
+//             },
+
+//             create: {
+//               ...profile
+//             },
+
+//             update: {
+//               ...profile
+//             }
+//           }
+//         }
+//       }
+//     })
+
+//     console.log("Upsert result: ", userTransaction)
+//     return userTransaction
+//     // const account = await tx.account.upsert({
+//     //   where: {
+//     //     userId: userObj.user.id
+//     //   },
+//     //   update: {
+//     //     type: userObj.account.type,
+//     //     provider: userObj.account.provider,
+//     //     providerAccountId: userObj.account.providerAccountId,
+//     //     refresh_token: userObj.account.refresh_token,
+//     //     access_token: userObj.account.access_token,
+//     //     expires_at: userObj.account.expires_at,
+//     //     token_type: userObj.account.token_type,
+//     //     scope: userObj.account.scope,
+//     //     id_token: userObj.account.id_token,
+//     //     session_state: userObj.account.session_state
+//     //   },
+//     //   create: {
+//     //     type: userObj.account.type,
+//     //     provider: userObj.account.provider,
+//     //     providerAccountId: userObj.account.providerAccountId,
+//     //     refresh_token: userObj.account.refresh_token,
+//     //     access_token: userObj.account.access_token,
+//     //     expires_at: userObj.account.expires_at,
+//     //     token_type: userObj.account.token_type,
+//     //     scope: userObj.account.scope,
+//     //     id_token: userObj.account.id_token,
+//     //     session_state: userObj.account.session_state,
+//     //     user: {
+//     //       ...userObj.user
+//     //     }
+//     //   }
+//     // })
+
+//     // const userByAccount = await getUserByAccount({
+//     //   providerAccountId: account.providerAccountId,
+//     //   provider: account.provider,
+//     // })
+
+//     // const newUser = await prismaAdapter.createUser(obj.user)
+
+//     // return await prismaAdapter.linkAccount({ ...obj.account, userId: newUser.id })
+//   }).then(result => {
+//     console.log("Setting cookie")
+//     const token = jwt.sign({data: result.id }, process.env.NEXTAUTH_SECRET, { expiresIn: '1h' });
+//     setCookie(NEW_USER_COOKIE, token, { req, res })
+//    // console.log("Set the cookie?")
+//     // console.log("obj: ", userObj)
+
+//     // const token = jwt.sign({data: userObj.user.id }, process.env.NEXTAUTH_SECRET, { expiresIn: '1h' });
+//     // setCookie(NEW_USER_COOKIE, token, { req, res })
+//   })
