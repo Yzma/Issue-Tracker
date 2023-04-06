06b962fcd1d8dfa6c4227d4a1f474cdc9e516f64
Yzma <10090913+Yzma@users.noreply.github.com>
2023-04-06 18:32:34 -0400

    Properly install fontawesome

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
 import { SessionProvider } from "next-auth/react";
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css"; 
+
 import "@/styles/globals.css";
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import "@/styles/popover-styles.css" // TODO: Move this into the actual component
+import "@/styles/popover-styles.css"
+
+config.autoAddCss = false;
 
 export default function App({
   Component,
