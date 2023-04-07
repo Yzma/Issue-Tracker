1dd7ddc5f59039ed18e6b693dfe6a3d6c9048d8a
Yzma <10090913+Yzma@users.noreply.github.com>
2023-04-07 01:31:09 -0400

    Cleanup original MarkdownEditor component

--- src/components/markdown/MarkdownEditor.js
+++ src/components/markdown/MarkdownEditor.js
@@ -1,7 +1,4 @@
-import React, { useState } from "react"
-
 import MarkdownIt from "markdown-it"
-
 import dynamic from "next/dynamic"
 
 import "react-markdown-editor-lite/lib/index.css"
@@ -13,96 +10,16 @@ const MdEditor = dynamic(() => import("react-markdown-editor-lite"), {
 const mdParser = new MarkdownIt({})
 
 const MarkdownEditor = (props) => {
-  const [text, setText] = useState(props.placeholder)
-
   return (
     <>
-      <div>
-        <MdEditor
-          value={text}
-          style={{ height: "350px" }}
-          renderHTML={(text) => mdParser.render(text)}
-          onChange={({ html, text }, event) => {
-            setText(text)
-            props.onChange?.(text)
-          }}
-          view={{ menu: true, md: true, html: false }}
-        />
-
-        {props.children}
-      </div>
+      <MdEditor
+        renderHTML={(text) => mdParser.render(text)}
+        view={{ menu: true, md: true, html: false }}
+        {...props}
+      />
+      {props.children}
     </>
   )
-
-  // return (
-  //   <>
-  //     <div className="w-full max-w-md px-2 py-5 sm:px-0">
-  //       <Tab2.Group>
-  //         <Tab2.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-  //           <Tab2
-  //             className={({ selected }) =>
-  //               classNames(
-  //                 "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
-  //                 "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
-  //                 selected
-  //                   ? "bg-white shadow"
-  //                   : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
-  //               )
-  //             }
-  //           >
-  //             Write
-  //           </Tab2>
-
-  //           <Tab2
-  //             className={({ selected }) =>
-  //               classNames(
-  //                 "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
-  //                 "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
-  //                 selected
-  //                   ? "bg-white shadow"
-  //                   : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
-  //               )
-  //             }
-  //           >
-  //             Preview
-  //           </Tab2>
-  //         </Tab2.List>
-  //         <Tab2.Panels className="mt-2">
-  //           <Tab2.Panel
-  //             className={classNames(
-  //               "rounded-xl bg-white p-3",
-  //               "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
-  //             )}
-  //           >
-  //             <MdEditor
-  //               value={text}
-  //               style={{ height: "350px" }}
-  //               renderHTML={(text) => mdParser.render(text)}
-  //               onChange={({ html, text }, event) => {
-  //                 setText(text)
-  //                 props.onChange?.(text)
-  //               }}
-  //               view={{ menu: true, md: true, html: false }}
-  //             />
-  //           </Tab2.Panel>
-
-  //           <Tab2.Panel
-  //             className={classNames(
-  //               "rounded-xl bg-white p-3",
-  //               "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
-  //             )}
-  //           >
-  //             <article class="prose md:prose-lg lg:prose-xl">
-  //               <ReactMarkdown remarkPlugins={[remarkGfm]}>
-  //                 {text}
-  //               </ReactMarkdown>
-  //             </article>
-  //           </Tab2.Panel>
-  //         </Tab2.Panels>
-  //       </Tab2.Group>
-  //       {props.children}
-  //     </div>
-  //   </>
-  // )
 }
+
 export default MarkdownEditor
