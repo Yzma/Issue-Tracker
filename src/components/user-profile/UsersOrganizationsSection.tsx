59e41b442e134261f7e6629fb61ae8f279f227ea
Yzma <10090913+Yzma@users.noreply.github.com>
2023-04-10 11:47:32 -0400

    Update UsersOrganizationsSection to map and render users orgs instead of placeholder

--- src/components/user-profile/UsersOrganizationsSection.tsx
+++ src/components/user-profile/UsersOrganizationsSection.tsx
@@ -1,24 +1,28 @@
-
-type Props = {
-  organizations: string[]
-}
-
+import Link from "next/link"
 import * as Avatar from "@radix-ui/react-avatar"
 
-export default function UsersOrganizationsSection(organizations: Props) {
-  return (
+import Organizations from "./types"
 
+export default function UsersOrganizationsSection({ organizations }: { organizations: Organizations[] }) {
+  return (
     <div className="flex flex-col gap-y-2 items-start justify-start left-0 text-left">
       <p className="text-xl font-bold">Organizations</p>
-      <Avatar.Root className="inline-flex hover:cursor-pointer h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded align-middle">
-        <Avatar.Fallback
-          className="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium"
-          delayMs={600}
-        >
-          A
-        </Avatar.Fallback>
-      </Avatar.Root>
-    </div>
 
+      {organizations.map((organization) => {
+        return (
+          <div key={organization.id}>
+            <Link href={`/${organization.name.toLowerCase()}`}>
+              <Avatar.Root className="inline-flex hover:cursor-pointer h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded align-middle">
+                <Avatar.Fallback
+                  className="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium"
+                >
+                  {organization.name.charAt(0).toUpperCase()}
+                </Avatar.Fallback>
+              </Avatar.Root>
+            </Link>
+          </div>
+        )
+      })}
+    </div>
   )
 }
---
+++ src/components/user-profile/types.ts
@@ -0,0 +1,5 @@
+
+export default interface Organizations {
+  id: string
+  name: string
+}
