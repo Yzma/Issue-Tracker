ec40f48763e8caf4da92bb77e9662ab6fd3a37c4
Yzma <10090913+Yzma@users.noreply.github.com>
2023-04-09 21:59:37 -0400

    Move Project list view into seperate Typescript components

---
+++ src/components/projects/ProjectItem.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { ProjectItem } from "./types";
+
+import moment from "moment"
+
+export default function ProjectList({ project }: { project: ProjectItem }) {
+  return (
+    <div>
+      <div>
+        <div className="flex flex-col gap-y-1 py-3">
+          <div>
+            <Link
+              href={`/${project.namespace}/${project.name}`}
+              className="inline text-2xl text-blue-600 hover:text-blue-900 hover:cursor-pointer hover:underline pr-1"
+            >
+              {project.name}
+            </Link>
+
+            <span
+              className="text-sm text-center font-semibold text-white px-1.5 bg-gray-700 rounded-full">
+              {project.private ? <>Private</> : <>Public</>}
+            </span>
+          </div>
+          <div>
+            <p className="text-base text-gray-600">{project.description}</p>
+          </div>
+          <div className="flex gap-x-3">
+            <p className="text-base text-gray-600">Created: {moment(project.updatedAt).format("MMM Do YY")}</p>
+            <p className="text-base text-gray-600">Last Updated: {moment(project.createdAt).fromNow()}</p>
+          </div>
+        </div>
+        <hr className="border-gray-300 mx-auto w-full" />
+      </div>
+    </div>
+  )
+}
