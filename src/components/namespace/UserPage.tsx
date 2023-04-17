409b3e876be848d212c074d9980671f1baf040ca
Yzma <10090913+Yzma@users.noreply.github.com>
2023-04-17 18:24:09 -0400

    Update UserPage to use types

--- src/components/namespace/UserPage.tsx
+++ src/components/namespace/UserPage.tsx
@@ -3,34 +3,25 @@ import Head from "next/head"
 import ProfileContainer from "@/components/user-profile/ProfileContainer"
 import ProjectList from "@/components/projects/ProjectList"
 
-import { ProfileInformation } from "../user-profile/types"
+import { UserProfileProps } from "@/types/types"
 
-const UserPage = ({ props }) => {
-  const projects = props.data.members
-    .filter((e) => e.organization === null)
-    .map((e) => {
-      return { 
-        ...e.project,
-        namespace: e.project.namespace.name
-      }
-    })
+const UserPage = ({ data }: { data: UserProfileProps }) => {
 
-    console.log("Props: ", props)
-  console.log("PROJECT ", projects)
+  console.log("Props: ", data)
 
-  const profileInformation: ProfileInformation = {
-    name: props.data.username,
-    username: props.data.username,
+  const profileInformation = {
+    name: data.user.username,
+    username: data.user.username,
     profilePictureURL: "",
-    bio: props.data.bio,
-    socialLinks: [],
+    bio: data.user.bio,
+    socialLinks: [data.user.socialLink1, data.user.socialLink2, data.user.socialLink3, data.user.socialLink4],
     organizations: []
   }
 
   return (
     <>
       <Head>
-        <title>{props.data.username}'s Profile</title>
+        <title>{data.user.username}'s Profile</title>
         <meta name="description" content="TODO: Description?" />
       </Head>
 
@@ -44,7 +35,7 @@ const UserPage = ({ props }) => {
           <div className="w-3/4 pl-8">
             <div className="flex flex-col h-full">
               <div className="w-full max-w-5xl mt-4 flex-1">
-                <ProjectList projects={projects} />
+                <ProjectList projects={[]} />
                 {/* <ProjectSection projects={projects} /> */}
               </div>
             </div>
