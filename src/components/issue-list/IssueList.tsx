1e4505a0cffb47077d4a5c29112fa71433739594
Yzma <10090913+Yzma@users.noreply.github.com>
2023-06-03 15:02:17 -0400

    Remove unused comments in IssueList component

--- src/components/issue-list/IssueList.tsx
+++ src/components/issue-list/IssueList.tsx
@@ -4,15 +4,7 @@ import IssueListBody from "./IssueListBody"
 import IssueListHeader from "./IssueListHeader"
 import { SearchFilters } from "@/types/types";
 import { Issue } from "./types";
-import { UseSearchFiltersHook, useSearchFilters } from "@/hooks/useSearchFilters";
-
-// export const IssueListContext = createContext<{ searchParams: SearchFilters, setSearchParams: Dispatch<SetStateAction<SearchFilters>> }>({
-//   searchParams: {
-//     open: true,
-//     sort: "newest"
-//   },
-//   setSearchParams: () => { },
-// });
+import { UseSearchFiltersHook } from "@/hooks/useSearchFilters";
 
 type ContextType<T extends SearchFilters> = {
   searchFilters: SearchFilters;
@@ -33,12 +25,6 @@ type IssueListProps = {
 };
 
 export function IssueList2({ issues, useSearchFiltersHook }: IssueListProps) {
-  console.log("Current search filters: ", useSearchFiltersHook.searchFilters)
-
-  // if(!issues) {
-  //   return <div>No issues found! Try creating some</div>
-  // }
-
   return (
     <div className="container mx-auto px-4 py-8 max-w-3/4">
       <div className="bg-white shadow-md rounded-md">
@@ -50,26 +36,3 @@ export function IssueList2({ issues, useSearchFiltersHook }: IssueListProps) {
     </div>
   )
 }
-
-
-// export default function IssueList({ issues, onUpdate }: { issues: Issue[], onUpdate: (searchParams: SearchFilters) => void }) {
-//   const [searchParams, setSearchParams] = useState<SearchFilters>({
-//     open: true,
-//     sort: "newest"
-//   })
-
-//   useEffect(() => { 
-//     onUpdate(searchParams);
-//   }, [searchParams])
-
-//   return (
-//     <div className="container mx-auto px-4 py-8 max-w-3/4">
-//       <div className="bg-white shadow-md rounded-md">
-//         <IssueListContext.Provider value={{ searchParams, setSearchParams }}>
-//           <IssueListHeader />
-//           <IssueListBody issues={issues} />
-//         </IssueListContext.Provider>
-//       </div>
-//     </div>
-//   )
-// }
