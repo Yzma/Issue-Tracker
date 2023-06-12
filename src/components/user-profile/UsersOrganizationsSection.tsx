d3967ebd0ec83fff1456c563daa90217735909cb
Yzma <10090913+Yzma@users.noreply.github.com>
2023-06-12 14:35:49 -0400

    Fix spacing issue for user organizations section on profile page

--- src/components/user-profile/UsersOrganizationsSection.tsx
+++ src/components/user-profile/UsersOrganizationsSection.tsx
@@ -11,22 +11,24 @@ export default function UsersOrganizationsSection({
   return (
     <div>
       {organizations.length > 0 && (
-        <div className="flex flex-col gap-y-2 items-start justify-start left-0 text-left">
+        <div className="flex flex-col gap-y-4 items-start justify-start left-0 text-left">
           <p className="text-xl font-bold">Organizations</p>
 
-          {organizations.map((organization) => {
-            return (
-              <div key={organization.name}>
-                <Link href={`/${organization.name.toLowerCase()}`}>
-                  <Avatar.Root className="inline-flex hover:cursor-pointer h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded align-middle">
-                    <Avatar.Fallback className="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium">
-                      {organization.name.charAt(0).toUpperCase()}
-                    </Avatar.Fallback>
-                  </Avatar.Root>
-                </Link>
-              </div>
-            )
-          })}
+          <div className="flex flex-row flex-wrap gap-x-2 gap-y-2">
+            {organizations.map((organization) => {
+              return (
+                <div key={organization.name}>
+                  <Link href={`/${organization.name.toLowerCase()}`}>
+                    <Avatar.Root className="inline-flex hover:cursor-pointer h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded align-middle">
+                      <Avatar.Fallback className="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium">
+                        {organization.name.charAt(0).toUpperCase()}
+                      </Avatar.Fallback>
+                    </Avatar.Root>
+                  </Link>
+                </div>
+              )
+            })}
+          </div>
         </div>
       )}
     </div>
