4af42e13b6871d9e96d9ac2234be8a3fbc692d67
Yzma <10090913+Yzma@users.noreply.github.com>
2023-06-13 01:17:19 -0400

    Update profile container to use trpc and react-hook-form

--- src/components/user-profile/ProfileContainer.tsx
+++ src/components/user-profile/ProfileContainer.tsx
@@ -6,16 +6,18 @@ import {
   useContext,
 } from 'react'
 
-import { Formik, Form, Field, FieldArray } from 'formik'
-
-import axios from 'axios'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { Session } from 'next-auth'
 import { useSession } from 'next-auth/react'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import UserSocialLinks from '@/components/user-profile/UserSocialLinks'
 import UsersOrganizationsSection from '@/components/user-profile/UsersOrganizationsSection'
 
 import { ProfileInformation } from './types'
+import { UserProfileSchema } from '@/lib/zod-schemas'
+import { trpc } from '@/lib/trpc'
 
 interface ProfileContextInterface {
   profile: ProfileInformation
@@ -24,43 +26,14 @@ interface ProfileContextInterface {
   session: Session
 }
 
+type UserProfileUpdateType = z.infer<typeof UserProfileSchema>
+
 const ProfileContext = createContext<ProfileContextInterface>({
   profile: null,
-  setProfile: () => { },
-  setEditing: () => { },
+  setProfile: () => {},
+  setEditing: () => {},
   session: null,
 })
-
-export default function ProfileContainer({
-  data,
-}: {
-  data: ProfileInformation
-}) {
-  const [editing, setEditing] = useState<boolean>(false)
-  const [profile, setProfile] = useState<ProfileInformation>({
-    ...data,
-    socialLinks: [...new Array(4)].map(
-      (_, index) => data.socialLinks[index] || ''
-    ),
-  })
-  const { data: session } = useSession()
-
-  return (
-    <ProfileContext.Provider
-      value={{ profile, setProfile, setEditing, session }}
-    >
-      <div className="flex flex-col gap-y-3">
-        <div className="flex items-center justify-center">
-          <div className="w-72 h-72 mb-4 bg-gray-200 text-gray-600 rounded-full" />
-        </div>
-        {editing ? <ProfileContainerEditor /> : <ProfileContainerViewer />}
-        <UserSocialLinks links={profile.socialLinks} />
-        <hr className="border-gray-300 my-4 mx-auto w-full" />
-        <UsersOrganizationsSection organizations={data.organizations} />
-      </div>
-    </ProfileContext.Provider>
-  )
-}
 
 function ProfileContainerViewer() {
   const { profile, setEditing, session } = useContext(ProfileContext)
@@ -79,7 +52,11 @@ function ProfileContainerViewer() {
             </p>
 
             {session?.user.namespace.name === profile.name && (
-              <button className="btn w-full" onClick={() => setEditing(true)}>
+              <button
+                type="button"
+                className="btn w-full"
+                onClick={() => setEditing(true)}
+              >
                 Edit Profile
               </button>
             )}
@@ -93,137 +70,172 @@ function ProfileContainerViewer() {
 function ProfileContainerEditor() {
   const { profile, setProfile, setEditing } = useContext(ProfileContext)
 
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<UserProfileUpdateType>({
+    resolver: zodResolver(UserProfileSchema),
+    defaultValues: {
+      socialLinks: profile.socialLinks,
+      bio: profile.bio,
+    },
+  })
+
+  const updateProjectMutation = trpc.users.updateProfile.useMutation({
+    onSuccess: (data) => {
+      setProfile((prevState) => ({
+        ...prevState,
+        bio: data.bio || undefined,
+        socialLinks: Array.of(
+          data.socialLink1,
+          data.socialLink2,
+          data.socialLink3,
+          data.socialLink4
+        ) as string[],
+      }))
+      setEditing(false)
+    },
+    onError: (error) => {
+      setError('root', { type: 'custom', message: error.message })
+    },
+  })
+
+  console.log('Form erros: ', errors)
+
+  const onSubmit: SubmitHandler<UserProfileUpdateType> = (formData) => {
+    console.log('formData', formData)
+    // return updateProjectMutation.mutate(formData)
+  }
+
   return (
     <div className="relative">
       <div className="flex flex-col">
-        <Formik
-          initialValues={{
-            name: profile.name,
-            bio: profile.bio || '',
-            socialLinks: profile.socialLinks,
-          }}
-          validateOnChange={false}
-          validateOnBlur={false}
-          onSubmit={(values, { setSubmitting, setFieldError }) => {
-            console.log('Submit values:', values)
-            axios
-              .put('/api/user', {
-                name: values.name,
-                bio: values.bio,
-                socialLinks: values.socialLinks,
-              })
-              .then((response) => {
-                console.log('RESPONSE:', response)
-                setProfile((prevState) => ({
-                  ...prevState,
-                  name: values.name,
-                  bio: values.bio,
-                  socialLinks: values.socialLinks,
-                }))
-                // router.push(`/${values.name}`)
-                setEditing(false)
-              })
-              .catch((error) => {
-                console.log('ERROR:', error.response.data)
-                console.log('ERROR:', error)
-              })
-              .finally(() => {
-                setSubmitting(false)
-              })
-          }}
-        >
-          {({ errors, isSubmitting, values, setFieldError, handleChange }) => (
-            <Form>
-              <div className="flex flex-col gap-y-0 text-left">
-                <div className="flex flex-col gap-y-3 pb-3">
-                  <div>
-                    <label className="block text-sm font-medium mb-1">
-                      Name
-                    </label>
-                    <Field name="name">
-                      {({ field, meta }) => (
-                        <div>
-                          <input
-                            className="form-input w-full"
-                            type="text"
-                            placeholder={profile.name}
-                            {...field}
-                          />
-                          {meta.touched && meta.error && (
-                            <div className="error">{meta.error}</div>
-                          )}
-                        </div>
-                      )}
-                    </Field>
-                  </div>
-
-                  <div>
-                    <label className="block text-sm font-medium mb-1">
-                      Bio
-                    </label>
-                    <Field name="bio">
-                      {({ field, meta }) => (
-                        <div>
-                          <textarea
-                            className="form-textarea w-full"
-                            placeholder="Add a bio"
-                            {...field}
-                          />
-                          {meta.touched && meta.error && (
-                            <div className="error">{meta.error}</div>
-                          )}
-                        </div>
-                      )}
-                    </Field>
-                  </div>
-
-                  <div>
-                    <label className="block text-sm font-medium mb-1">
-                      Social Accounts
-                    </label>
-                    <div className="flex flex-col gap-y-2">
-                      <FieldArray
-                        name="socialLinks"
-                        render={(arrayHelpers) => (
-                          <>
-                            {values.socialLinks.map((link, index) => {
-                              return (
-                                <div key={index}>
-                                  <Field
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                    type="text"
-                                    name={`socialLinks.${index}`}
-                                    placeholder="Link to social profile"
-                                  />
-                                </div>
-                              )
-                            })}
-                          </>
-                        )}
-                      />
-                    </div>
-                  </div>
+        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-y-0 text-left">
+            {errors.bio?.message && (
+              <div className="py-3">
+                <div className="flex w-full px-4 py-2 rounded-sm text-sm border bg-rose-100 border-rose-200 text-rose-600">
+                  <div>You must enter a valid bio!</div>
                 </div>
               </div>
-              <div className="flex flex-row gap-x-1">
-                <button
-                  className="btn-xs h-8 w-14 bg-emerald-500 hover:bg-emerald-600 text-white"
-                  type="submit"
-                >
-                  Save
-                </button>
-                <button
-                  className="btn-xs h-8 w-14 bg-gray-500 hover:bg-gray-600 text-white"
-                  type="button"
-                  onClick={() => setEditing(false)}
-                >
-                  Cancel
-                </button>
+            )}
+            <div className="flex flex-col gap-y-3 pb-3">
+              <div>
+                <label className="block text-sm font-medium mb-1" htmlFor="bio">
+                  Bio
+                  <textarea
+                    className="form-textarea w-full min-h-16"
+                    placeholder="Add a bio"
+                    // eslint-disable-next-line react/jsx-props-no-spreading
+                    {...register('bio')}
+                  />
+                </label>
               </div>
-            </Form>
-          )}
-        </Formik>
+
+              <div>
+                <label
+                  className="block text-sm font-medium mb-1"
+                  htmlFor="socialLinks"
+                >
+                  Social Accounts
+                  <div className="flex flex-col gap-y-2">
+                    {errors.socialLinks && (
+                      <div className="py-3">
+                        <div className="flex w-full px-4 py-2 rounded-sm text-sm border bg-rose-100 border-rose-200 text-rose-600">
+                          <div>All social links must be valid URLs!</div>
+                        </div>
+                      </div>
+                    )}
+                    {/* <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      type="text"
+                      placeholder="Link to social profile"
+                      // eslint-disable-next-line react/jsx-props-no-spreading
+                      {...register(`socialLinks.0`, {
+                        setValueAs(value: string) {
+                          if (!value) return undefined
+                          return value
+                        },
+                      })}
+                    /> */}
+                    {profile.socialLinks &&
+                      profile.socialLinks.map((link, index) => {
+                        return (
+                          // eslint-disable-next-line react/no-array-index-key
+                          <div key={index}>
+                            <input
+                              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                              type="text"
+                              placeholder="Link to social profile"
+                              // eslint-disable-next-line react/jsx-props-no-spreading
+                              {...register(`socialLinks.${index}`, {
+                                setValueAs(value: string) {
+                                  if (!value) return undefined
+                                  return value
+                                },
+                              })}
+                            />
+                          </div>
+                        )
+                      })}
+                  </div>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row gap-x-1">
+            <button
+              className="btn-xs h-8 w-14 bg-emerald-500 hover:bg-emerald-600 text-white"
+              type="submit"
+              disabled={
+                isSubmitting ||
+                updateProjectMutation.isLoading ||
+                updateProjectMutation.isError
+              }
+            >
+              Save
+            </button>
+            <button
+              className="btn-xs h-8 w-14 bg-gray-500 hover:bg-gray-600 text-white"
+              type="button"
+              onClick={() => setEditing(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
       </div>
     </div>
+  )
+}
+
+export default function ProfileContainer({
+  data,
+}: {
+  data: ProfileInformation
+}) {
+  const [editing, setEditing] = useState<boolean>(false)
+  const [profile, setProfile] = useState<ProfileInformation>({
+    ...data,
+  })
+  const { data: session } = useSession()
+
+  return (
+    <ProfileContext.Provider
+      value={{ profile, setProfile, setEditing, session }}
+    >
+      <div className="flex flex-col gap-y-3">
+        <div className="flex items-center justify-center">
+          <div className="w-72 h-72 mb-4 bg-gray-200 text-gray-600 rounded-full" />
+        </div>
+        {editing ? <ProfileContainerEditor /> : <ProfileContainerViewer />}
+        <UserSocialLinks links={profile.socialLinks} />
+        <hr className="border-gray-300 my-4 mx-auto w-full" />
+        <UsersOrganizationsSection organizations={data.organizations} />
+      </div>
+    </ProfileContext.Provider>
   )
 }
