79befa19ff0cb35ec4267fa9fa6c8ba6504d42a3
Yzma <10090913+Yzma@users.noreply.github.com>
2023-07-16 15:58:39 -0400

    Fix item highlight for below navbar

--- src/components/navbar/BelowNavbar.tsx
+++ src/components/navbar/BelowNavbar.tsx
@@ -1,19 +1,20 @@
-import React, { createContext, useContext } from 'react'
+import React, { PropsWithChildren, createContext, useContext } from 'react'
 
 import Link from 'next/link'
 
-export default function BelowNavbar({ children }) {
-  return <>{children}</>
+export default function BelowNavbar({ children }: PropsWithChildren) {
+  return <div>{children}</div>
 }
 
-function BreadcrumbLinks({ children }) {
+function BreadcrumbLinks({ children }: PropsWithChildren) {
   return (
     <div className="px-4 sm:px-6 lg:px-8 pt-7 pb-1 w-full max-w-9xl mx-auto">
       <div className="flex flex-row pl-2">
         {React.Children.map(children, (child, index) => (
           <>
             {child}
-            {children.length > 1 &&
+            {children &&
+              children.length > 1 &&
               index !== children.length - 1 &&
               children.length !== 1 && (
                 <p className="text-2xl px-1 font-light">/</p>
@@ -27,7 +28,7 @@ function BreadcrumbLinks({ children }) {
 
 BelowNavbar.BreadcrumbLinks = BreadcrumbLinks
 
-function BreadcrumbLink({ children }) {
+function BreadcrumbLink({ children }: PropsWithChildren) {
   return (
     <p className="text-2xl text-blue-600 hover:text-blue-900 hover:underline">
       {children}
@@ -39,10 +40,13 @@ BelowNavbar.BreadcrumbLink = BreadcrumbLink
 
 const NavbarContext = createContext({ selected: '' })
 
-function MenuList({ children, selected = '' }) {
+function MenuList({
+  children,
+  selected = '',
+}: PropsWithChildren<{ selected: string }>) {
   return (
     <NavbarContext.Provider value={{ selected }}>
-      <div className="px-4 sm:px-6 lg:px-8 pt-6 pb-2 w-full max-w-9xl mx-auto border-b-2 border-gray-400">
+      <div className="px-4 sm:px-6 lg:px-8 pt-6 pb-2 w-full max-w-9xl mx-auto border-b-2 border-gray-400 z-10">
         <div className="flex flex-row gap-x-4">{children}</div>
       </div>
     </NavbarContext.Provider>
@@ -56,10 +60,11 @@ function MenuListItem({ children, id, href }) {
   console.log(`Selected from context (${selected})`)
   return (
     <Link href={href}>
-      <div
-        className={`flex flex-row px-2 py-2 rounded hover:bg-gray-300 ${selected === id && 'border-b-4 border-indigo-500'
-          }`}
-      >
+      <div className="flex flex-row px-2 py-2 rounded hover:bg-gray-300 relative">
+        {selected === id && (
+          <div className="absolute top-[calc(50%+27px)] left-0 w-full h-[4px] rounded-md bg-orange-500 z-30" />
+          //  top-11
+        )}
         {children}
       </div>
     </Link>
