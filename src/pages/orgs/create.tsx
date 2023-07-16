d85f660516fc697b6b0c61a8617d659ee85ef791
Yzma <10090913+Yzma@users.noreply.github.com>
2023-07-16 13:18:08 -0400

    Refactor organization create page

--- src/pages/orgs/create.tsx
+++ src/pages/orgs/create.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { SubmitHandler, useForm, useWatch } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { trpc } from '@/lib/trpc/trpc'
-import Header from '@/components/Header'
 import { NamespaceSchema } from '@/lib/zod-schemas'
+import DefaultLayout from '@/components/ui/DefaultLayout'
+import { Button } from '@/components/ui/button'
 
 type OrganizationCreationType = z.infer<typeof NamespaceSchema>
 
@@ -18,8 +19,14 @@ export default function OrganizationCreate() {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
+    control,
   } = useForm<OrganizationCreationType>({
     resolver: zodResolver(NamespaceSchema),
+  })
+
+  const organizationName = useWatch({
+    control,
+    name: 'name',
   })
 
   const onSubmit: SubmitHandler<OrganizationCreationType> = (data) => {
@@ -34,71 +41,59 @@ export default function OrganizationCreate() {
   return (
     <>
       <Head>
-        <title>Create new Organization</title>
+        <title>Create a new Organization</title>
       </Head>
-
-      <Header />
-
-      <main className="flex absolute inset-0  bg-slate-100 justify-center items-center">
-        <div className="relative px-4 sm:px-6 lg:px-8 pb-8  max-w-lg mx-auto">
-          <div className="bg-white px-8 pb-6 pt-9 rounded-b shadow-lg">
-            <div className="text-center mb-6">
-              <h1 className="text-xl leading-snug text-slate-800 font-semibold mb-2">
-                Create a new Organization
-              </h1>
-              <div className="text-sm">Tell us about your organization</div>
-            </div>
-
-            <div>
-              <div className="space-y-4">
-                {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-                <form onSubmit={handleSubmit(onSubmit)}>
-                  {errors.name?.message && (
-                    <div className="py-3">
-                      <div className="flex w-full px-4 py-2 rounded-sm text-sm border bg-rose-100 border-rose-200 text-rose-600">
-                        <div>You must enter a valid organization name!</div>
-                      </div>
-                    </div>
-                  )}
-
-                  <div>
-                    <label
-                      className="block text-sm font-medium mb-1"
-                      htmlFor="name"
-                    >
-                      Organization Name <span className="text-rose-500">*</span>
-                      <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="text"
-                        // eslint-disable-next-line react/jsx-props-no-spreading
-                        {...register('name')}
-                      />
-                    </label>
-                  </div>
-
-                  <div className="text-sm pt-4">
-                    <p>
-                      This will be the name of your account on Issue Tracker.
-                    </p>
-                  </div>
-
-                  <div className="mt-6">
-                    <div className="mb-4">
-                      <button
-                        className="btn bg-indigo-500 hover:bg-indigo-600 text-white"
-                        type="submit"
-                        disabled={isSubmitting}
-                      >
-                        Create Organization
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
+      <DefaultLayout>
+        <main className="flex flex-col justify-center items-center pt-6 mt-6">
+          <div className="text-center mb-6">
+            <div className="text-sm">Tell us about your organization</div>
+            <h1 className="text-3xl leading-snug text-slate-800 font-semibold mb-2">
+              Set up your organization
+            </h1>
           </div>
-        </div>
-      </main>
+
+          <div className="w-2/5 space-y-4 mt-4">
+            {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+            <form onSubmit={handleSubmit(onSubmit)}>
+              {errors.name?.message && (
+                <div className="py-3">
+                  <div className="flex w-full px-4 py-2 rounded-sm text-sm border bg-rose-100 border-rose-200 text-rose-600">
+                    <div>You must enter a valid organization name!</div>
+                  </div>
+                </div>
+              )}
+
+              <div>
+                <label
+                  className="block text-sm font-medium mb-1"
+                  htmlFor="name"
+                >
+                  Organization Name <span className="text-rose-500">*</span>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    type="text"
+                    // eslint-disable-next-line react/jsx-props-no-spreading
+                    {...register('name')}
+                  />
+                </label>
+              </div>
+
+              <div className="text-sm pt-2">
+                <p>This will be the name of your account on Issue Tracker.</p>
+                <p>
+                  Your URL will be: https://issue-tracker.com/{organizationName}
+                </p>
+              </div>
+
+              <div className="flex justify-end pt-4">
+                <Button type="submit" disabled={isSubmitting}>
+                  Create Organization
+                </Button>
+              </div>
+            </form>
+          </div>
+        </main>
+      </DefaultLayout>
     </>
   )
 }
