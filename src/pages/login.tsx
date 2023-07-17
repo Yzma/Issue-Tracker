2052f216550f6a94908f3cc3aefbe6d855d786cf
Yzma <10090913+Yzma@users.noreply.github.com>
2023-07-17 17:10:51 -0400

    Small cleanup

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -44,7 +44,7 @@ export default function SignIn({
       <div className="relative md:flex">
         <div className="md:w-1/2 relative">
           <Link
-            href="/examples/authentication"
+            href="/"
             className={cn(
               buttonVariants({ variant: 'default' }),
               'absolute left-10 top-4 md:left-8 md:top-8 w-20'
@@ -138,42 +138,6 @@ export default function SignIn({
       </div>
     </>
   )
-}
-
-{
-  /* 
-<div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-          <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-            <div className="absolute inset-0 bg-zinc-900" />
-            <div className="relative z-20 flex items-center text-lg font-medium">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-6 w-6"
-              >
-                <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-              </svg>
-              Acme Inc
-            </div>
-            <div className="relative z-20 mt-auto">
-              <blockquote className="space-y-2">
-                <p className="text-lg">
-                  &ldquo;This library has saved me countless hours of work and
-                  helped me deliver stunning designs to my clients faster than
-                  ever before.&rdquo;
-                </p>
-                <footer className="text-sm">Sofia Davis</footer>
-              </blockquote>
-            </div>
-          </div>
-        </div>
-      </div> 
-    */
 }
 
 export const getServerSideProps: GetServerSideProps<{
