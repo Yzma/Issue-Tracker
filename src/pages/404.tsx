653a82393c7c716e7c2801d97787e60ffe0ece31
Yzma <10090913+Yzma@users.noreply.github.com>
2023-07-20 10:50:40 -0400

    Update 404 pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Header from '@/components/Header'
+import DefaultLayout from '@/components/ui/DefaultLayout'
+import { Button } from '@/components/ui/button'
 
 export default function NotFound() {
   return (
@@ -12,32 +14,17 @@ export default function NotFound() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-
-      <main>
-        <div className="flex h-screen overflow-hidden">
-          <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-              <div className="max-w-2xl m-auto mt-16">
-                <div className="text-center px-4">
-                  <div className="inline-flex mb-8">
-                    <p className="text-9xl font-light text-gray-500">404</p>
-                  </div>
-                  <div className="mb-6">
-                    The requested page does not exist...
-                  </div>
-                  <Link
-                    href="/"
-                    className="btn bg-indigo-500 hover:bg-indigo-600 text-white"
-                  >
-                    Back To Home
-                  </Link>
-                </div>
-              </div>
-            </div>
+      <DefaultLayout>
+        <div className="text-center px-4">
+          <div className="inline-flex mb-8">
+            <p className="text-9xl font-light text-gray-500">404</p>
           </div>
+          <div className="mb-6">The requested page does not exist...</div>
+          <Button asChild>
+            <Link href="/">Back To Home</Link>
+          </Button>
         </div>
-      </main>
+      </DefaultLayout>
     </>
   )
 }
