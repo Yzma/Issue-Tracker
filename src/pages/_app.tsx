f0c2b037a3679d53613c367d1062a9e8c5e36705
Yzma <10090913+Yzma@users.noreply.github.com>
2023-08-03 21:46:03 -0400

    Small change to meta tag in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,10 +32,7 @@ function MyApp({
   return (
     <SessionProvider session={session as Session}>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {layout}
