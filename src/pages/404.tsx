b742c0e4cf5bc0ea3346fffacfa21e193237aa28
Yzma <10090913+Yzma@users.noreply.github.com>
2023-08-06 17:32:23 -0400

    Update 404 page to use new layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Header from '@/components/Header'
-import DefaultLayout from '@/components/ui/DefaultLayout'
 import { Button } from '@/components/ui/button'
+import { getLayout } from '@/components/layout/DefaultLayoutTest'
 
 export default function NotFound() {
   return (
@@ -10,21 +9,19 @@ export default function NotFound() {
       <Head>
         <title>404 - Not Found</title>
         <meta name="description" content="404 - Page not found" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <DefaultLayout>
-        <div className="text-center px-4">
-          <div className="inline-flex mb-8">
-            <p className="text-9xl font-light text-gray-500">404</p>
-          </div>
-          <div className="mb-6">The requested page does not exist...</div>
-          <Button asChild>
-            <Link href="/">Back To Home</Link>
-          </Button>
+      <div className="text-center px-4">
+        <div className="inline-flex mb-8">
+          <p className="text-9xl font-light text-gray-500">404</p>
         </div>
-      </DefaultLayout>
+        <div className="mb-6">The requested page does not exist...</div>
+        <Button asChild>
+          <Link href="/">Back To Home</Link>
+        </Button>
+      </div>
     </>
   )
 }
+
+NotFound.getLayout = getLayout
