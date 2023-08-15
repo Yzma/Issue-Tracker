e0c71785411eb0a4e9917c7af4ea609cee3a186c
Yzma <10090913+Yzma@users.noreply.github.com>
2023-08-14 21:46:26 -0400

    Cleanup invites page

--- src/pages/invites.tsx
+++ src/pages/invites.tsx
@@ -1,20 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark, faBug } from '@fortawesome/free-solid-svg-icons'
-
 import moment from 'moment/moment'
-import * as Dialog from '@radix-ui/react-dialog'
-
-import { useSession } from 'next-auth/react'
-import { ArrowUpDown, MoreHorizontal, ChevronDown } from 'lucide-react'
+import { MoreHorizontal } from 'lucide-react'
 import {
   ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
@@ -26,7 +17,6 @@ import { getLayout } from '@/components/layout/DefaultLayoutTest'
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -37,162 +27,15 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
-import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { Badge } from '@/components/ui/badge'
+import { RouterOutputs, trpc } from '@/lib/trpc/trpc'
+import { GetElementType } from '@/types/types'
 
-const paymentData: Payment[] = [
-  {
-    id: 'm5gr84i9',
-    amount: 316,
-    status: 'success',
-    email: 'ken99@yahoo.com',
-  },
-  {
-    id: '3u1reuv4',
-    amount: 242,
-    status: 'success',
-    email: 'Abe45@gmail.com',
-  },
-  {
-    id: 'derv1ws0',
-    amount: 837,
-    status: 'processing',
-    email: 'Monserrat44@gmail.com',
-  },
-  {
-    id: '5kma53ae',
-    amount: 874,
-    status: 'success',
-    email: 'Silas22@gmail.com',
-  },
-  {
-    id: 'bhqecj4p',
-    amount: 721,
-    status: 'failed',
-    email: 'carmella@hotmail.com',
-  },
-]
-
-export type Payment = {
-  id: string
-  amount: number
-  status: 'pending' | 'processing' | 'success' | 'failed'
-  email: string
-}
-
-type InviteType =
-  | {
-      type: 'organization'
-      organizationName: string
-    }
-  | {
-      type: 'project'
-      projectName: string
-      projectNamespace: string
-    }
-
-// type Invite = {
-//   id: string
-//   name: string
-//   role: string
-//   createdAt: Date
-//   invitedBy: string
-// }
-
-// const data: Invite[] = [
-//   {
-//     id: 'm5gr84i9',
-//     type: 'Organization',
-//     invitedAt: new Date(),
-//     invitedBy: 'Andrew',
-//     name: 'Project Name',
-//     role: 'ADMIN',
-//   },
-//   {
-//     id: '123',
-//     type: 'Project',
-//     invitedAt: new Date(),
-//     invitedBy: 'Andrew',
-//     name: 'Project Name',
-//     role: 'ADMIN',
-//   },
-// ]
-
-/// /
-///
-
-// select: {
-//   id: true,
-//   role: true,
-//   createdAt: true,
-//   inviteeUser: {
-//     select: {
-//       username: true
-//     }
-//   },
-//   organization: {
-//     select: {
-//       name: true
-//     }
-//   },
-//   project: {
-//     select: {
-//       name: true,
-//       namespace: {
-//         select: {
-//           name: true
-
-type Invite = {
-  id: string
-  role: string
-  createdAt: Date
-  inviteeUser: {
-    username: string
-  }
-  organization?: {
-    name: string
-  }
-  project?: {
-    name: string
-    namespace: {
-      name: string
-    }
-  }
-}
-
-const data: Invite[] = [
-  {
-    id: 'orgId',
-    role: 'ADMIN',
-    createdAt: new Date(),
-    inviteeUser: {
-      username: 'Andrew',
-    },
-    organization: {
-      name: 'OrgName',
-    },
-  },
-  {
-    id: 'projectId',
-    role: 'ADMIN',
-    createdAt: new Date(),
-    inviteeUser: {
-      username: 'Andrew',
-    },
-    project: {
-      name: 'ProjectName',
-      namespace: {
-        name: 'Max',
-      },
-    },
-  },
-]
+type Invite = GetElementType<RouterOutputs['users']['getInvites']>
 
 export const columns: ColumnDef<Invite>[] = [
   {
@@ -203,13 +46,6 @@ export const columns: ColumnDef<Invite>[] = [
   {
     accessorKey: 'type',
     header: 'Type',
-    // accessorFn: (row) => {
-    //   return row.type === 'Organization' ? (
-    //     <Label>asd</Label>
-    //   ) : (
-    //     <Label>asd</Label>
-    //   )
-    // },
     cell: ({ row }) =>
       row.original.organization ? (
         <Badge className="bg-green-400 hover:bg-green-400">Organization</Badge>
@@ -232,6 +68,7 @@ export const columns: ColumnDef<Invite>[] = [
       ) : (
         <Link
           className="text-blue-500 hover:text-blue-900 hover:cursor-pointer hover:underline pr-1"
+          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
           href={`/${row.original.project?.namespace.name}/${row.original.project?.name}`}
         >
           {row.original.project?.name}
@@ -242,14 +79,14 @@ export const columns: ColumnDef<Invite>[] = [
     accessorKey: 'invitedBy',
     header: 'Invited By',
     cell: ({ row }) => {
-      const value = row.original
       return (
         <div className="capitalize">
           <Link
             className="text-blue-500 hover:text-blue-900 hover:cursor-pointer hover:underline pr-1"
-            href={`/${row.original.inviteeUser.username}`}
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+            href={`/${row.original.inviteeUser?.username}`}
           >
-            {row.original.inviteeUser.username}
+            {row.original.inviteeUser?.username}
           </Link>
         </div>
       )
@@ -270,44 +107,13 @@ export const columns: ColumnDef<Invite>[] = [
     header: 'Role',
     cell: ({ row }) => <div className="capitalize">{row.getValue('role')}</div>,
   },
-  // {
-  //   accessorKey: 'email',
-  //   header: ({ column }) => {
-  //     return (
-  //       <Button
-  //         variant="ghost"
-  //         onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-  //       >
-  //         Email
-  //         <ArrowUpDown className="ml-2 h-4 w-4" />
-  //       </Button>
-  //     )
-  //   },
-  //   cell: ({ row }) => <div className="lowercase">{row.getValue('email')}</div>,
-  // },
-  // {
-  //   accessorKey: 'amount',
-  //   header: () => <div className="text-right">Amount</div>,
-  //   cell: ({ row }) => {
-  //     const amount = parseFloat(row.getValue('amount'))
-
-  //     // Format the amount as a dollar amount
-  //     const formatted = new Intl.NumberFormat('en-US', {
-  //       style: 'currency',
-  //       currency: 'USD',
-  //     }).format(amount)
-
-  //     return <div className="text-right font-medium">{formatted}</div>
-  //   },
-  // },
   {
     id: 'actions',
     enableHiding: false,
-    header: ({ column }) => {
+    header: () => {
       return <div className="w-2" />
     },
-    cell: ({ row }) => {
-      // const payment = row.original
+    cell: () => {
       return (
         <div className="flex justify-end">
           <DropdownMenu>
@@ -330,21 +136,14 @@ export const columns: ColumnDef<Invite>[] = [
 ]
 
 export default function MyInvites() {
-  const { data: session } = useSession()
-
-  const [rowSelection, setRowSelection] = React.useState({})
-
+  const getInvitesQuery = trpc.users.getInvites.useQuery()
   const table = useReactTable({
-    data,
+    data: getInvitesQuery.data || [],
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
-    onRowSelectionChange: setRowSelection,
-    state: {
-      rowSelection,
-    },
   })
 
   return (
@@ -355,14 +154,7 @@ export default function MyInvites() {
 
       <div className="w-full">
         <div className="flex items-center py-4">
-          <Input
-            placeholder="Filter emails..."
-            // value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
-            // onChange={(event) =>
-            //   table.getColumn('email')?.setFilterValue(event.target.value)
-            // }
-            className="max-w-sm"
-          />
+          <Input placeholder="Filter emails..." className="max-w-sm" />
         </div>
         <div className="rounded-md border">
           <Table>
@@ -415,225 +207,6 @@ export default function MyInvites() {
           </Table>
         </div>
       </div>
-      {/* <Dialog.Root open={open} onOpenChange={setOpen}>
-        <Dialog.Portal>
-          <Dialog.Overlay className="DialogOverlay" />
-          <Dialog.Content className="DialogContent">
-            <Dialog.Title className="DialogTitle">Confirmation</Dialog.Title>
-            <Dialog.Description className="DialogDescription">
-              Are you sure you want to decline this invitation?
-            </Dialog.Description>
-            <div
-              className="gap-2"
-              style={{
-                display: 'flex',
-                marginTop: 25,
-                justifyContent: 'flex-end',
-              }}
-            >
-              <Dialog.Close asChild>
-                <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
-                  Cancel
-                </button>
-              </Dialog.Close>
-              <Dialog.Close asChild>
-                <button
-                  className="btn bg-red-500 hover:bg-red-600 text-white"
-                  onClick={() => declineInvitation(open.id)}
-                >
-                  Confirm
-                </button>
-              </Dialog.Close>
-            </div>
-            <Dialog.Close asChild>
-              <button className="IconButton" aria-label="Close">
-                <FontAwesomeIcon icon={faXmark} />
-              </button>
-            </Dialog.Close>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
-
-      <Dialog.Root open={openAccept} onOpenChange={setOpenAccept}>
-        <Dialog.Portal>
-          <Dialog.Overlay className="DialogOverlay" />
-          <Dialog.Content className="DialogContent">
-            <Dialog.Title className="DialogTitle">Confirmation</Dialog.Title>
-            <Dialog.Description className="DialogDescription">
-              Are you sure you want to accept this invitation?
-            </Dialog.Description>
-            <div
-              className="gap-2"
-              style={{
-                display: 'flex',
-                marginTop: 25,
-                justifyContent: 'flex-end',
-              }}
-            >
-              <Dialog.Close asChild>
-                <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
-                  Cancel
-                </button>
-              </Dialog.Close>
-              <Dialog.Close asChild>
-                <button
-                  className="btn bg-red-500 hover:bg-red-600 text-white"
-                  onClick={() => acceptInvitation(openAccept.id)}
-                >
-                  Confirm
-                </button>
-              </Dialog.Close>
-            </div>
-            <Dialog.Close asChild>
-              <button className="IconButton" aria-label="Close">
-                <FontAwesomeIcon icon={faXmark} />
-              </button>
-            </Dialog.Close>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root> */}
-
-      {/* <div>
-            <div className="overflow-x-auto">
-              <table className="table-auto w-full rounded-xl shadow-lg">
-                <thead className="text-xs font-semibold uppercase text-slate-500 bg-slate-50 border-t border-b border-slate-200">
-                  <tr>
-                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                      <div className="font-semibold text-left">#</div>
-                    </th>
-                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                      <div className="font-semibold">Type</div>
-                    </th>
-                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                      <div className="font-semibold">Name</div>
-                    </th>
-                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                      <div className="font-semibold">Invited By</div>
-                    </th>
-                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                      <div className="font-semibold">Invited At</div>
-                    </th>
-                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                      <div className="font-semibold">Role</div>
-                    </th>
-                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                      <span className="sr-only">Menu</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="text-sm divide-y divide-slate-200">
-                  {props.invites.map((invite, index) => (
-                    <tr key={index}>
-                      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                        <div className="text-left">{index + 1}</div>
-                      </td>
-
-                      {invite.organization ? (
-                        <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                          <div className="text-center">Organization</div>
-                        </td>
-                      ) : (
-                        <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                          <div className="text-center">Project</div>
-                        </td>
-                      )}
-
-                      {invite.organization ? (
-                        <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                          <div className="text-center">
-                            <Link
-                              className="text-sky-400 hover:text-sky-700"
-                              href={`/${invite.organization.name}`}
-                            >
-                              {invite.organization.name}
-                            </Link>
-                          </div>
-                        </td>
-                      ) : (
-                        <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                          <div className="text-center">
-                            <Link
-                              className="text-sky-400 hover:text-sky-700"
-                              href={`/${invite.project.namespace.name}/${invite.project.name}`}
-                            >
-                              {invite.project.name}
-                            </Link>
-                          </div>
-                        </td>
-                      )}
-
-                      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                        <div className="text-center">
-                          <Link
-                            className="text-sky-400 hover:text-sky-700"
-                            href={`/${invite.inviteeUser.username}`}
-                          >
-                            {invite.inviteeUser.username}
-                          </Link>
-                        </div>
-                      </td>
-
-                      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                        <div className="text-center">
-                          {moment(invite.createdAt).format('MMM Do YY')}
-                        </div>
-                      </td>
-
-                      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                        <div className="text-center">{invite.role}</div>
-                      </td>
-
-                      <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-px">
-                        <DropdownMenu.Root>
-                          <DropdownMenu.Trigger asChild>
-                            <svg
-                              className="w-8 h-8 fill-current "
-                              viewBox="0 0 32 32"
-                            >
-                              <circle cx="16" cy="16" r="2" />
-                              <circle cx="10" cy="16" r="2" />
-                              <circle cx="22" cy="16" r="2" />
-                            </svg>
-                          </DropdownMenu.Trigger>
-
-                          <DropdownMenu.Portal>
-                            <DropdownMenu.Content
-                              className="DropdownMenuContent"
-                              sideOffset={5}
-                            >
-                              <DropdownMenu.Item
-                                className="DropdownMenuItem"
-                                onClick={() =>
-                                  setOpen({
-                                    id: invite.id,
-                                  })
-                                }
-                              >
-                                Decline Invitation
-                                <div className="RightSlot" />
-                              </DropdownMenu.Item>
-
-                              <DropdownMenu.Item
-                                className="DropdownMenuItem"
-                                onClick={() =>
-                                  setOpenAccept({
-                                    id: invite.id,
-                                  })
-                                }
-                              >
-                                Accept Invitation
-                                <div className="RightSlot" />
-                              </DropdownMenu.Item>
-                            </DropdownMenu.Content>
-                          </DropdownMenu.Portal>
-                        </DropdownMenu.Root>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div> */}
     </>
   )
 }
@@ -651,59 +224,3 @@ MyInvites.getLayout = getLayout
 //       },
 //     }
 //   }
-
-//   const invites = await prisma.member.findMany({
-//     where: {
-//       inviteeId: session.user.id,
-//     },
-
-//     select: {
-//       id: true,
-//       role: true,
-//       createdAt: true,
-//       inviteeUser: {
-//         select: {
-//           username: true,
-//         },
-//       },
-//       organization: {
-//         select: {
-//           name: true,
-//         },
-//       },
-//       project: {
-//         select: {
-//           name: true,
-//           namespace: {
-//             select: {
-//               name: true,
-//             },
-//           },
-//         },
-//       },
-//     },
-//   })
-
-//   if (!invites) {
-//     return {
-//       props: {
-//         invites: [],
-//       },
-//     }
-//   }
-
-//   console.log(invites)
-
-//   const mapped = invites.map((e) => {
-//     return {
-//       ...e,
-//       createdAt: e.createdAt.toISOString(),
-//     }
-//   })
-
-//   return {
-//     props: {
-//       invites: mapped,
-//     },
-//   }
-// }
