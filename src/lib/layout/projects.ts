4c80abd90c8b5fbe64ded399ef63604f25cadeec
Yzma <10090913+Yzma@users.noreply.github.com>
2023-08-15 19:01:32 -0400

    Implement getProjectServerSideProps

---
+++ src/lib/layout/projects.ts
@@ -0,0 +1,41 @@
+import { GetServerSidePropsContext } from 'next'
+import ssrHelper from '../trpc/ssrHelper'
+
+// TODO: This could be moved once we change the context creation
+export async function getProjectServerSideProps(
+  context: GetServerSidePropsContext
+) {
+  const namespaceName = context.params?.namespaceName
+  const projectName = context.params?.projectName
+
+  if (
+    namespaceName === undefined ||
+    typeof namespaceName !== 'string' ||
+    projectName === undefined ||
+    typeof projectName !== 'string'
+  )
+    throw new Error('no slug')
+
+  console.log('namespaceName', namespaceName)
+  console.log('projectName', projectName)
+  const helpers = await ssrHelper(context)
+  return helpers.projects.getProject
+    .fetch({
+      name: projectName,
+      owner: namespaceName,
+    })
+    .then(() => {
+      return {
+        props: {
+          trpcState: helpers.dehydrate(),
+          namespaceName,
+          projectName,
+        },
+      }
+    })
+    .catch(() => {
+      return {
+        notFound: true,
+      }
+    })
+}
