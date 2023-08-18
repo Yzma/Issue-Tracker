05aa2310aa39b015b8ad8bf42590a8aeb9541012
Yzma <10090913+Yzma@users.noreply.github.com>
2023-08-18 19:23:36 -0400

    Get basic frontend done for projects label page

--- src/pages/[namespaceName]/[projectName]/labels.tsx
+++ src/pages/[namespaceName]/[projectName]/labels.tsx
@@ -2,26 +2,181 @@ import Head from 'next/head'
 
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { getProjectLayout } from '@/components/layout/project/ProjectLayout'
 import { getProjectServerSideProps } from '@/lib/layout/projects'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Skeleton } from '@/components/ui/skeleton'
+import { trpc } from '@/lib/trpc/trpc'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Badge } from '@/components/ui/badge'
+
+type Label = {
+  name: string
+  color: string
+  description: string
+}
+
+export const columns: ColumnDef<Label>[] = [
+  {
+    accessorKey: 'index',
+    header: '#',
+    cell: ({ row }) => <div className="capitalize">{row.index + 1}</div>,
+    size: 1,
+  },
+  {
+    accessorKey: 'name',
+    cell: ({ row }) => (
+      <div className="capitalize">
+        <Badge
+          style={{
+            color: 'white',
+            background: `#${row.original.color}`,
+          }}
+        >
+          {row.original.name}
+        </Badge>
+      </div>
+    ),
+  },
+  {
+    accessorKey: 'description',
+    cell: ({ row }) => (
+      <div className="capitalize">{row.original.description}</div>
+    ),
+  },
+  {
+    id: 'actions',
+    enableHiding: false,
+    header: () => {
+      return <div className="w-2" />
+    },
+    cell: () => {
+      return (
+        <div className="flex justify-end space-x-2">
+          <Button size="sm">Edit</Button>
+          <Button size="sm" variant="destructive">
+            Delete
+          </Button>
+        </div>
+      )
+    },
+  },
+]
 
 export default function ProjectLabels({
   projectName,
+  namespaceName,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const labelsQuery = trpc.projects.getLabels.useQuery({
+    name: projectName,
+    owner: namespaceName,
+  })
+  const table = useReactTable({
+    data: labelsQuery.data || [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+  })
+
+  console.log('data: ', labelsQuery.data)
   return (
     <>
       <Head>
         <title>{projectName} Labels</title>
       </Head>
 
-      <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-        <header className="px-5 py-4">
-          <h2 className="font-bold text-slate-800">Labels</h2>
-        </header>
-        <div>
-          {/* <LabelSearchBar onSearch={handleSearch} /> */}
+      <div className="w-full">
+        <div className="flex items-center justify-between py-4">
+          <Input
+            className="w-1/3"
+            type="search"
+            placeholder="Search for an issue"
+          />
 
-          {/* <LabelList labels={filteredLabels} /> */}
+          <Button asChild>
+            <div className="hover:cursor-pointer">
+              <FontAwesomeIcon className="mr-2" icon={faPlus} />
+              <span>New Label</span>
+            </div>
+          </Button>
+        </div>
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Labels ({labelsQuery.data?.length})</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {/* eslint-disable-next-line no-nested-ternary */}
+              {labelsQuery.isLoading ? (
+                <>
+                  <TableRow>
+                    <TableCell colSpan={0} className="h-24 text-center">
+                      <div className="flex flex-row h-20 justify-between items-center">
+                        <Skeleton className="h-12 mx-4 w-full" />
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell colSpan={0} className="h-24 text-center">
+                      <div className="flex flex-row h-20 justify-between items-center">
+                        <Skeleton className="h-12 mx-4 w-full" />
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell colSpan={0} className="h-24 text-center">
+                      <div className="flex flex-row h-20 justify-between items-center">
+                        <Skeleton className="h-12 mx-4 w-full" />
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                </>
+              ) : table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow key={row.id}>
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={0} className="h-24 text-center">
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
         </div>
       </div>
     </>
