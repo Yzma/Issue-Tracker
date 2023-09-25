867caac9a4ca4ac405f677a6794911501d5d9a42
Yzma <10090913+Yzma@users.noreply.github.com>
2023-09-25 12:42:51 -0400

    Cleanup project creation page to use Form components and properly show errors

--- src/pages/projects/create.tsx
+++ src/pages/projects/create.tsx
@@ -14,6 +14,26 @@ import { trpc } from '@/lib/trpc/trpc'
 import ssrHelper from '@/lib/trpc/ssrHelper'
 import DefaultLayout from '@/components/ui/DefaultLayout'
 import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  FormRequiredField,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Separator } from '@/components/ui/separator'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { Skeleton } from '@/components/ui/skeleton'
 
 type ProjectCreationType = z.infer<typeof ProjectCreationSchema>
 
@@ -24,13 +44,13 @@ export default function ProjectCreate() {
   const userOrganizations = trpc.users.getOrganizations.useQuery()
   const { data: session } = useSession()
 
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors, isSubmitting },
-  } = useForm<ProjectCreationType>({
+  const form = useForm<ProjectCreationType>({
     resolver: zodResolver(ProjectCreationSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+      visibility: 'public',
+    },
   })
 
   const onSubmit: SubmitHandler<ProjectCreationType> = (data) => {
@@ -39,9 +59,12 @@ export default function ProjectCreate() {
       .then((response) =>
         router.push(`/${response.namespace}/${response.name}`)
       )
-      .catch((error) => {
-        setError('name', { type: 'custom', message: 'custom message' }) // TODO: Set proper error message
-        // console.log('ERROR:', error) // TODO: remove this
+      .catch(() => {
+        form.setError('owner', {
+          type: 'custom',
+          message:
+            'Failed to create project. Please contact support if the error persists.',
+        })
       })
   }
 
@@ -49,17 +72,17 @@ export default function ProjectCreate() {
     if (!userOrganizations.data || !session) return []
     return (
       <>
-        <option
+        <SelectItem
           key={session.user.namespace.name}
           value={session.user.namespace.name}
         >
           {session.user.namespace.name}
-        </option>
+        </SelectItem>
         {userOrganizations.data.map((organization) => {
           return (
-            <option key={organization.name} value={organization.name}>
+            <SelectItem key={organization.name} value={organization.name}>
               {organization.name}
-            </option>
+            </SelectItem>
           )
         })}
       </>
@@ -75,7 +98,7 @@ export default function ProjectCreate() {
       <DefaultLayout>
         <div className="flex flex-col items-center justify-center">
           <div>
-            <div className="border-b border-gray-300 pb-2">
+            <div className="pb-2">
               <h2 className="mb-1 text-3xl font-bold leading-snug text-slate-800">
                 Create a new project
               </h2>
@@ -84,124 +107,171 @@ export default function ProjectCreate() {
               </div>
             </div>
 
+            <Separator />
+
             <div className="space-y-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-4">
-              {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-              <form onSubmit={handleSubmit(onSubmit)}>
-                {errors.name?.message && (
-                  <div className="py-3">
-                    <div className="flex w-full rounded-sm border border-rose-200 bg-rose-100 px-4 py-2 text-sm text-rose-600">
-                      <div>You must enter a valid username!</div>
+              {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+              <Form {...form}>
+                {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+                <form
+                  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                  onSubmit={form.handleSubmit(onSubmit)}
+                >
+                  <section className="flex flex-row py-4">
+                    <FormField
+                      control={form.control}
+                      name="owner"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>
+                            Owner <FormRequiredField />
+                          </FormLabel>
+                          <Select onValueChange={field.onChange}>
+                            <FormControl>
+                              <SelectTrigger className="w-64">
+                                <SelectValue placeholder="Chose an owner" />
+                              </SelectTrigger>
+                            </FormControl>
+                            {!options && (
+                              <SelectContent>
+                                <SelectItem disabled value="1">
+                                  <Skeleton className="h-3.5 w-52" />
+                                </SelectItem>
+                                <SelectItem disabled value="2">
+                                  <Skeleton className="h-3.5 w-52" />
+                                </SelectItem>
+                                <SelectItem disabled value="3">
+                                  <Skeleton className="h-3.5 w-52" />
+                                </SelectItem>
+                              </SelectContent>
+                            )}
+                            {options && (
+                              <SelectContent>{options}</SelectContent>
+                            )}
+                          </Select>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <div className="auto-cols-auto px-3 pt-8 text-2xl font-semibold">
+                      /
                     </div>
-                  </div>
-                )}
 
-                <section className="flex flex-row border-b border-gray-300 py-4">
-                  <div className="sm:w-1/3">
-                    <label
-                      className="mb-1 block text-sm font-medium"
-                      htmlFor="owner"
-                    >
-                      Owner <span className="text-rose-500">*</span>
-                      <select
-                        className="form-input w-full"
-                        // eslint-disable-next-line react/jsx-props-no-spreading
-                        {...register('owner')}
-                      >
-                        {options}
-                      </select>
-                    </label>
-                  </div>
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({ field }) => (
+                        <FormItem className="w-full">
+                          <FormLabel>
+                            Project Name <FormRequiredField />
+                          </FormLabel>
+                          <FormControl>
+                            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+                            <Input {...field} className="py-0" />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </section>
 
-                  <div className="auto-cols-auto px-3 pt-6 text-2xl font-semibold">
-                    /
-                  </div>
+                  <Separator />
 
-                  <div className="grow sm:w-1/3">
-                    <label
-                      className="mb-1 block text-sm font-medium"
-                      htmlFor="name"
-                    >
-                      Project Name <span className="text-rose-500">*</span>
-                      <input
-                        className="form-input w-full"
-                        type="text"
-                        // eslint-disable-next-line react/jsx-props-no-spreading
-                        {...register('name')}
-                      />
-                    </label>
-                  </div>
-                </section>
+                  <section className="py-4">
+                    <FormField
+                      control={form.control}
+                      name="description"
+                      render={({ field }) => (
+                        <FormItem className="w-full">
+                          <FormLabel className="mb-1 flex gap-x-1 align-middle leading-snug">
+                            <h2 className="text-xl font-bold text-slate-800">
+                              Description
+                            </h2>
+                            <p className="pt-1 font-light">(optional)</p>
+                          </FormLabel>
+                          <FormControl>
+                            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+                            <Input {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </section>
 
-                <section className="border-b border-gray-300 py-4">
-                  <div className="mb-1 flex gap-x-1 align-middle leading-snug">
-                    <h2 className="text-xl font-bold text-slate-800">
-                      Description
+                  <Separator />
+
+                  <section className="py-4">
+                    <h2 className="mb-4 text-xl font-bold leading-snug text-slate-800">
+                      Visibility
                     </h2>
-                    <p className="pt-1 font-thin">(optional)</p>
-                  </div>
 
-                  <div className="mt-5 flex grow flex-wrap">
-                    <div className="mr-2 grow">
-                      <input
-                        className="form-input w-full"
-                        type="text"
-                        // eslint-disable-next-line react/jsx-props-no-spreading
-                        {...register('description')}
-                      />
-                    </div>
-                  </div>
-                </section>
+                    <FormField
+                      control={form.control}
+                      name="visibility"
+                      render={({ field }) => (
+                        <FormItem className="space-y-3">
+                          <RadioGroup
+                            onValueChange={field.onChange}
+                            defaultValue="public"
+                            className="flex flex-col space-y-1"
+                          >
+                            <FormItem className="flex items-center space-x-3 space-y-0">
+                              <FormControl>
+                                <RadioGroupItem id="public" value="public" />
+                              </FormControl>
+                              <FontAwesomeIcon
+                                icon={faBookBookmark}
+                                size="lg"
+                              />
+                              <div>
+                                <p className="font-bold">Public</p>
+                                <p className="text-sm font-light">
+                                  Anyone on the internet can see this project.
+                                </p>
+                              </div>
+                            </FormItem>
+                            <FormItem className="flex items-center space-x-3 space-y-0">
+                              <FormControl>
+                                <RadioGroupItem id="private" value="private" />
+                              </FormControl>
+                              <FontAwesomeIcon icon={faLock} size="lg" />
+                              <div>
+                                <p className="font-bold">Private</p>
+                                <p className="text-sm font-light">
+                                  Only invited users and users that are part of
+                                  the same organization the project is created
+                                  will be able to view the project
+                                </p>
+                              </div>
+                            </FormItem>
+                          </RadioGroup>
 
-                <section className="border-b border-gray-300 py-4">
-                  <h2 className="mb-4 text-xl font-bold leading-snug text-slate-800">
-                    Visibility
-                  </h2>
-
-                  <div className="flew-row flex items-center gap-x-4 pb-3">
-                    <input
-                      // eslint-disable-next-line react/jsx-props-no-spreading
-                      {...register('visibility')}
-                      className="form-radio"
-                      type="radio"
-                      value="public"
-                      defaultChecked
+                          <FormMessage />
+                        </FormItem>
+                      )}
                     />
-                    <FontAwesomeIcon icon={faBookBookmark} size="lg" />
-                    <div>
-                      <p className="font-bold">Public</p>
-                      <p className="text-sm font-light">
-                        Anyone on the internet can see this project.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="flew-row flex items-center gap-x-4">
-                    <input
-                      // eslint-disable-next-line react/jsx-props-no-spreading
-                      {...register('visibility')}
-                      className="form-radio"
-                      type="radio"
-                      value="private"
-                    />
-                    <FontAwesomeIcon icon={faLock} size="lg" />
-                    <div>
-                      <p className="font-bold">Private</p>
-                      <p className="text-sm font-light">
-                        Only invited users and users that are part of the same
-                        organization the project is created will be able to view
-                        the project
-                      </p>
-                    </div>
-                  </div>
-                </section>
+                  </section>
 
-                <div className="flex flex-col border-t border-slate-200 py-5">
-                  <div className="flex self-end">
-                    <Button type="submit" disabled={isSubmitting}>
-                      Create Project
-                    </Button>
+                  <Separator />
+
+                  <div className="flex flex-col py-5">
+                    <div className="flex self-end">
+                      <Button
+                        type="submit"
+                        disabled={
+                          form.formState.isSubmitting ||
+                          createProjectMutation.isLoading ||
+                          !options
+                        }
+                      >
+                        Create Project
+                      </Button>
+                    </div>
                   </div>
-                </div>
-              </form>
+                </form>
+              </Form>
             </div>
           </div>
         </div>
