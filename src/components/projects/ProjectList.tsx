27921eb65435fbc8daf556cc0d478179c7cfe0d6
Andrew <10090913+Yzma@users.noreply.github.com>
2023-09-30 00:33:48 -0400

    Split ProjectList into multiple components and update UserPage and OrganizationPage (#138)

--- src/components/projects/ProjectList.tsx
+++ src/components/projects/ProjectList.tsx
@@ -1,113 +1,135 @@
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
-import ProjectItem from './ProjectItem'
-import { Input } from '../ui/input'
-import { Button } from '../ui/button'
-import { Project } from '@/server/routers/common'
+import { faBookBookmark } from '@fortawesome/free-solid-svg-icons'
+import moment from 'moment'
+import { cn } from '@/lib/utils'
+import { Badge } from '../ui/badge'
+import { ProjectItemType } from './types'
+import { Button, ButtonProps } from '../ui/button'
+import { Separator } from '../ui/separator'
+import { CustomLink } from '../ui/common'
+import { Input, InputProps } from '../ui/input'
 
-type ProjectListProps = {
-  projects: Project[]
-  projectCreationButton: JSX.Element | undefined
-}
-
-export default function ProjectList({
-  projects,
-  projectCreationButton,
-}: ProjectListProps) {
+const ProjectList = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
   return (
-    <div>
-      {projects.length === 0 ? (
-        <div className="flex flex-col items-center justify-center gap-y-5 p-10 text-4xl">
-          No projects found
-          {projectCreationButton !== undefined && projectCreationButton}
-        </div>
-      ) : (
-        <>
-          <div className="mb-5 flex content-start gap-x-1 pt-6">
-            <div className="flex w-full gap-x-2 gap-y-2 max-md:flex-col">
-              <Input
-                id="app-search"
-                type="search"
-                placeholder="Find a project..."
-              />
+    <div
+      className={cn('content-start gap-x-1 pt-6', className)}
+      ref={ref}
+      {...props}
+    />
+  )
+})
+ProjectList.displayName = 'ProjectList'
 
-              <div className="flex flex-row gap-x-2">
-                <div>
-                  <DropdownMenu.Root>
-                    <DropdownMenu.Trigger asChild>
-                      <Button size="sm">
-                        Type
-                        <FontAwesomeIcon icon={faCaretDown} className="pl-1" />
-                      </Button>
-                    </DropdownMenu.Trigger>
-
-                    <DropdownMenu.Portal>
-                      <DropdownMenu.Content
-                        className="z-20 min-w-[220px] rounded-md bg-white p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
-                        sideOffset={5}
-                        side="bottom"
-                        alignOffset={3}
-                      >
-                        <DropdownMenu.Item className="group relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none text-violet11 outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[disabled]:text-mauve8 data-[highlighted]:text-violet1">
-                          All
-                        </DropdownMenu.Item>
-
-                        <DropdownMenu.Item className="group relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none text-violet11 outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[disabled]:text-mauve8 data-[highlighted]:text-violet1">
-                          Private
-                        </DropdownMenu.Item>
-
-                        <DropdownMenu.Item className="group relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none text-violet11 outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[disabled]:text-mauve8 data-[highlighted]:text-violet1">
-                          Public
-                        </DropdownMenu.Item>
-                      </DropdownMenu.Content>
-                    </DropdownMenu.Portal>
-                  </DropdownMenu.Root>
-                </div>
-
-                <div>
-                  <DropdownMenu.Root>
-                    <DropdownMenu.Trigger asChild>
-                      <Button size="sm">
-                        Sort
-                        <FontAwesomeIcon icon={faCaretDown} className="pl-1" />
-                      </Button>
-                    </DropdownMenu.Trigger>
-
-                    <DropdownMenu.Portal>
-                      <DropdownMenu.Content
-                        className="z-20 min-w-[220px] rounded-md bg-white p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
-                        sideOffset={5}
-                        side="bottom"
-                        alignOffset={3}
-                      >
-                        <DropdownMenu.Item className="group relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none text-violet11 outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[disabled]:text-mauve8 data-[highlighted]:text-violet1">
-                          Name
-                        </DropdownMenu.Item>
-
-                        <DropdownMenu.Item className="group relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none text-violet11 outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[disabled]:text-mauve8 data-[highlighted]:text-violet1">
-                          Last Updated
-                        </DropdownMenu.Item>
-                      </DropdownMenu.Content>
-                    </DropdownMenu.Portal>
-                  </DropdownMenu.Root>
-                </div>
-              </div>
-            </div>
-
-            {projectCreationButton !== undefined && (
-              <div className="flex flex-row content-start align-top">
-                {projectCreationButton}
-              </div>
-            )}
-          </div>
-
-          <hr className="mx-auto w-full border-gray-300" />
-          {projects.map((project) => {
-            return <ProjectItem key={project.id} project={project} />
-          })}
-        </>
-      )}
+const ProjectListHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <div className="w-full space-y-2">
+      <div
+        className={cn('flex w-full gap-x-2 gap-y-2 max-md:flex-col', className)}
+        ref={ref}
+        {...props}
+      />
+      <Separator />
     </div>
   )
+})
+ProjectListHeader.displayName = 'ProjectListHeader'
+
+const ProjectListInputSearch = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className }, ref) => {
+    return (
+      <Input
+        ref={ref}
+        type="search"
+        placeholder="Find a project..."
+        className={cn(className)}
+      />
+    )
+  }
+)
+ProjectListInputSearch.displayName = 'ProjectListInputSearch'
+
+const ProjectListInputButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <Button ref={ref} size="sm" className={cn(className)} {...props}>
+        {children}
+      </Button>
+    )
+  }
+)
+ProjectListInputButton.displayName = 'ProjectListInputButton'
+
+// This could be split up into multiple smaller sub-components, but since this never changes, it will be left as is for now.
+const ProjectListItem = React.forwardRef<HTMLDivElement, ProjectItemType>(
+  ({ project }, ref) => {
+    return (
+      <div
+        className="mx-auto flex w-full flex-col gap-y-1 border-b border-gray-300 py-6"
+        ref={ref}
+      >
+        <div className="flex gap-x-1.5">
+          <CustomLink
+            href={`/${project.namespace}/${project.name}`}
+            className="pr-1 text-2xl"
+          >
+            {project.name}
+          </CustomLink>
+
+          <div className="flex pt-2 align-middle">
+            <Badge className="h-5 bg-gray-700">
+              {project.private ? <>Private</> : <>Public</>}
+            </Badge>
+          </div>
+        </div>
+        <div>
+          <p className="text-base text-gray-600">{project.description}</p>
+        </div>
+        <div className="flex gap-x-3">
+          <p className="text-base text-gray-600">
+            Last Updated: {moment(project.createdAt).fromNow()}
+          </p>
+        </div>
+      </div>
+    )
+  }
+)
+ProjectListItem.displayName = 'ProjectListItem'
+
+const ProjectListEmpty = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, children, ...props }, ref) => {
+  return (
+    <div
+      className={cn(
+        'flex flex-col items-center justify-center gap-y-5 p-10 text-4xl',
+        className
+      )}
+      ref={ref}
+      {...props}
+    >
+      <FontAwesomeIcon size="lg" icon={faBookBookmark} />
+      No projects found
+      {children}
+    </div>
+  )
+})
+ProjectListEmpty.displayName = 'ProjectListEmpty'
+
+export {
+  ProjectList,
+  ProjectListHeader,
+  ProjectListItem,
+  ProjectListEmpty,
+  ProjectListInputSearch,
+  ProjectListInputButton,
 }
---
+++ src/components/projects/SharedProjectList.tsx
@@ -0,0 +1,103 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookBookmark, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
+import {
+  ProjectList,
+  ProjectListEmpty,
+  ProjectListHeader,
+  ProjectListInputButton,
+  ProjectListInputSearch,
+  ProjectListItem,
+} from './ProjectList'
+import { ProjectListLoading } from './ProjectListLoading'
+import { SharedProjectListProps } from './types'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+import { Button } from '../ui/button'
+
+export default function SharedProjectList({
+  loading,
+  createProjectLink,
+  projects,
+}: SharedProjectListProps) {
+  return (
+    <div>
+      {loading ? (
+        <ProjectListLoading />
+      ) : (
+        <ProjectList>
+          {projects.length === 0 ? (
+            <ProjectListEmpty>
+              {createProjectLink && (
+                <Button className="flex w-48" size="lg" asChild>
+                  <Link href={createProjectLink}>
+                    <FontAwesomeIcon
+                      size="lg"
+                      icon={faBookBookmark}
+                      className="pr-3"
+                    />
+                    Create Project
+                  </Link>
+                </Button>
+              )}
+            </ProjectListEmpty>
+          ) : (
+            <ProjectListHeader>
+              <ProjectListInputSearch />
+
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <ProjectListInputButton>
+                    Type
+                    <FontAwesomeIcon icon={faCaretDown} className="pl-1" />
+                  </ProjectListInputButton>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuPortal>
+                  <DropdownMenuContent
+                    sideOffset={5}
+                    side="bottom"
+                    alignOffset={3}
+                  >
+                    <DropdownMenuItem>All</DropdownMenuItem>
+                    <DropdownMenuItem>Private</DropdownMenuItem>
+                    <DropdownMenuItem>Public</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenuPortal>
+              </DropdownMenu>
+
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <ProjectListInputButton>
+                    Sort
+                    <FontAwesomeIcon icon={faCaretDown} className="pl-1" />
+                  </ProjectListInputButton>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuPortal>
+                  <DropdownMenuContent
+                    sideOffset={5}
+                    side="bottom"
+                    alignOffset={3}
+                  >
+                    <DropdownMenuItem>Name</DropdownMenuItem>
+                    <DropdownMenuItem>Last Updated</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenuPortal>
+              </DropdownMenu>
+            </ProjectListHeader>
+          )}
+          {projects &&
+            projects.map((project) => {
+              return <ProjectListItem key={project.name} project={project} />
+            })}
+        </ProjectList>
+      )}
+    </div>
+  )
+}
